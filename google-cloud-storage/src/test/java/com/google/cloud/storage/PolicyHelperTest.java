938c9fe1373d0f5de8a5fca125a6b090f53b9b71
Frank Natividad <franknatividad@google.com>
2020-02-11 15:04:37 -0800

    wip

--- google-cloud-storage/src/test/java/com/google/cloud/storage/PolicyHelperTest.java
+++ google-cloud-storage/src/test/java/com/google/cloud/storage/PolicyHelperTest.java
@@ -41,6 +41,7 @@ public class PolicyHelperTest {
                 Identity.user("test1@gmail.com"),
                 Identity.user("test2@gmail.com"))
             .setEtag(ETAG)
+            .setVersion(1)
             .build();
     com.google.api.services.storage.model.Policy apiPolicy =
         new com.google.api.services.storage.model.Policy()
@@ -53,14 +54,15 @@ public class PolicyHelperTest {
                         .setMembers(
                             ImmutableList.of("user:test1@gmail.com", "user:test2@gmail.com"))
                         .setRole("roles/storage.objectAdmin")))
-            .setEtag(ETAG);
+            .setEtag(ETAG)
+            .setVersion(1);
 
-//    Policy actualLibPolicy = PolicyHelper.convertFromApiPolicy(apiPolicy);
-//    com.google.api.services.storage.model.Policy actualApiPolicy =
-//        PolicyHelper.convertToApiPolicy(libPolicy);
-//
-//    assertEquals(libPolicy, actualLibPolicy);
-//    assertTrue(new ApiPolicyMatcher(apiPolicy).matches(actualApiPolicy));
+    Policy actualLibPolicy = PolicyHelper.convertFromApiPolicy(apiPolicy);
+    com.google.api.services.storage.model.Policy actualApiPolicy =
+        PolicyHelper.convertToApiPolicy(libPolicy);
+
+    assertEquals(libPolicy, actualLibPolicy);
+    assertTrue(new ApiPolicyMatcher(apiPolicy).matches(actualApiPolicy));
   }
 
   @Test(expected = IllegalStateException.class)
