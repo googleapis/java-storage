9ed9d1389e92766b66e2b8b4fb78b44d96d98803
Frank Natividad <frankyn@users.noreply.github.com>
2021-03-08 12:30:27 -0800

    fix: npe in createFrom (#746)
    * fix: npe in create-from
    
    * address NPE issue
    
    * revert test it
    
    * address unit tests

--- google-cloud-storage/src/main/java/com/google/cloud/storage/BlobWriteChannel.java
+++ google-cloud-storage/src/main/java/com/google/cloud/storage/BlobWriteChannel.java
@@ -25,6 +25,7 @@ import com.google.cloud.RestorableState;
 import com.google.cloud.RetryHelper;
 import com.google.cloud.WriteChannel;
 import com.google.cloud.storage.spi.v1.StorageRpc;
+import com.google.common.collect.Maps;
 import java.net.URL;
 import java.util.Map;
 import java.util.concurrent.Callable;
@@ -77,7 +78,9 @@ class BlobWriteChannel extends BaseWriteChannel<StorageOptions, BlobInfo> {
   }
 
   private StorageObject getRemoteStorageObject() {
-    return getOptions().getStorageRpcV1().get(getEntity().toPb(), null);
+    return getOptions()
+        .getStorageRpcV1()
+        .get(getEntity().toPb(), Maps.newEnumMap(StorageRpc.Option.class));
   }
 
   private StorageException unrecoverableState(
