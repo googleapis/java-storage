88b5169cc3c391c75b326c263db73f7fbc2bd741
Emily Ball <emilyball@google.com>
2021-05-03 10:05:23 -0700

    test: adding xrefs
    testing adding before rolling out to all libraries - will roll back

--- .kokoro/release/publish_javadoc11.sh
+++ .kokoro/release/publish_javadoc11.sh
@@ -49,6 +49,11 @@ pushd target/docfx-yml
 python3 -m docuploader create-metadata \
  --name ${NAME} \
  --version ${VERSION} \
+ --xrefs devsite://java/gax \
+ --xrefs devsite://java/google-cloud-core \
+ --xrefs devsite://java/api-common \
+ --xrefs devsite://java/proto-google-common-protos \
+ --xrefs devsite://java/google-api-client \
  --language java
 
 # upload yml to production bucket
