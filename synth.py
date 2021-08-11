0ea286b335ef98feb3ae00437357d62eb2a917bd
Noah Dietz <noahdietz@users.noreply.github.com>
2021-08-11 13:59:44 -0700

    chore: add config and maven skeleton for generation of storage v2 gapic client (#922)
    Co-authored-by: BenWhitehead <BenWhitehead@users.noreply.github.com>

--- synth.py
+++ synth.py
@@ -14,9 +14,22 @@
 
 """This script is used to synthesize generated parts of this library."""
 
+import os
+import synthtool as s
 import synthtool.languages.java as java
 
 AUTOSYNTH_MULTIPLE_COMMITS = True
+
+service = 'storage'
+version = 'v2'
+
+library = java.bazel_library(
+    service=service,
+    version=version,
+    proto_path=f'google/{service}/{version}',
+    bazel_target=f'//google/{service}/{version}:google-cloud-{service}-{version}-java',
+    preserve_gapic=True,
+)
 
 java.common_templates(excludes=[
   '.kokoro/nightly/integration.cfg',
@@ -24,3 +37,11 @@ java.common_templates(excludes=[
   'CONTRIBUTING.md'
 ])
 
+gapic_main_dir = f'gapic-google-cloud-{service}-{version}/src/main'
+gapic_resource_dir = gapic_main_dir + '/resources/com/google/storage/v2'
+if not os.path.exists(gapic_resource_dir):
+  os.makedirs(gapic_resource_dir)
+os.rename(
+    gapic_main_dir + '/java/com/google/storage/v2/gapic_metadata.json',
+    gapic_resource_dir + '/gapic_metadata.json',
+)
