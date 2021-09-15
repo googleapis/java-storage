df473cdc954bb6b5d3e0092759bc59e96ede384b
BenWhitehead <BenWhitehead@users.noreply.github.com>
2021-09-15 17:50:50 -0400

    docs: add javadocs to Retrying.java

--- google-cloud-storage/src/main/java/com/google/cloud/storage/Retrying.java
+++ google-cloud-storage/src/main/java/com/google/cloud/storage/Retrying.java
@@ -29,12 +29,16 @@ import java.util.function.Function;
 final class Retrying {
 
   /**
-   * A convenience wrapper around {@link com.google.cloud.RetryHelper#runWithRetries(Callable, RetrySettings, ResultRetryAlgorithm, ApiClock)}
-   * that gives us centralized error translation and reduces some duplication in how we resolved
-   * the {@link RetrySettings} and {@link ApiClock}.
-   * @param options The {@link StorageOptions} which {@link RetrySettings} and {@link ApiClock} will be resolved from.
-   * @param c The {@link Callable} which will be passed to runWithRetries producing some {@code T}, can optionally return null
-   * @param f A post process mapping {@link Function} which can be used to transform the result from {@code c} if it is successful and non-null
+   * A convenience wrapper around {@link com.google.cloud.RetryHelper#runWithRetries(Callable,
+   * RetrySettings, ResultRetryAlgorithm, ApiClock)} that gives us centralized error translation and
+   * reduces some duplication in how we resolved the {@link RetrySettings} and {@link ApiClock}.
+   *
+   * @param options The {@link StorageOptions} which {@link RetrySettings} and {@link ApiClock} will
+   *     be resolved from.
+   * @param c The {@link Callable} which will be passed to runWithRetries producing some {@code T},
+   *     can optionally return null
+   * @param f A post process mapping {@link Function} which can be used to transform the result from
+   *     {@code c} if it is successful and non-null
    * @param <T> The result type of {@code c}
    * @param <U> The result type of any mapping that takes place via {@code f}
    * @return A {@code U} (possibly null) after applying {@code f} to the result of {@code c}
