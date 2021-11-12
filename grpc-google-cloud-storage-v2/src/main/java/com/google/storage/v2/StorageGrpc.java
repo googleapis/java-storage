a0a4c98c6b3d4791c2058b6b1d3e8023d66c8f43
yoshi-automation <yoshi-automation@google.com>
2021-11-12 14:50:15 -0800

    chore: update Java and Python dependencies
    PiperOrigin-RevId: 408420890
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Mon Nov 8 13:03:45 2021 -0800
    Source-Repo: googleapis/googleapis
    Source-Sha: 2921f9fb3bfbd16f6b2da0104373e2b47a80a65e
    Source-Link: https://github.com/googleapis/googleapis/commit/2921f9fb3bfbd16f6b2da0104373e2b47a80a65e

--- grpc-google-cloud-storage-v2/src/main/java/com/google/storage/v2/StorageGrpc.java
+++ grpc-google-cloud-storage-v2/src/main/java/com/google/storage/v2/StorageGrpc.java
@@ -44,6 +44,7 @@ import static io.grpc.MethodDescriptor.generateFullMethodName;
 @javax.annotation.Generated(
     value = "by gRPC proto compiler",
     comments = "Source: google/storage/v2/storage.proto")
+@io.grpc.stub.annotations.GrpcGenerated
 public final class StorageGrpc {
 
   private StorageGrpc() {}
