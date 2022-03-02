969e8f8c1e09bc211ecd1968837fc58b068d5b23
Mridula Peddada <mpeddada@google.com>
2022-03-01 19:45:45 -0500

    docs(sample): Add Native Image Sample

---
+++ samples/native-image-sample/src/main/java/com/example/storage/NativeImageStorageSample.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.storage;
+
+import com.google.cloud.BatchResult.Callback;
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.BucketInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageBatch;
+import com.google.cloud.storage.StorageException;
+import com.google.cloud.storage.StorageOptions;
+import java.util.UUID;
+
+/**
+ * Sample Storage application compiled with Native Image.
+ */
+public class NativeImageStorageSample {
+
+  static String BUCKET_NAME = "nativeimage-sample-bucket-" + UUID.randomUUID();
+  static String FILENAME = "nativeimage-sample-file-" + UUID.randomUUID();
+
+  /**
+   * Runs the storage sample application.
+   */
+  public static void main(String[] args) {
+
+    Storage storageClient = StorageOptions.getDefaultInstance().getService();
+
+    try {
+      createBucket(storageClient, BUCKET_NAME);
+      createFile(storageClient, BUCKET_NAME, FILENAME);
+      runBatchOperations(storageClient, BUCKET_NAME, FILENAME);
+    } finally {
+      System.out.println("Deleting resources.");
+      storageClient.delete(BUCKET_NAME, FILENAME);
+      storageClient.delete(BUCKET_NAME);
+    }
+  }
+
+  private static void runBatchOperations(
+      Storage storageClient, String bucketName, String fileName) {
+    BlobId blobId = BlobId.of(bucketName, fileName);
+
+    StorageBatch batch = storageClient.batch();
+    batch.update(BlobInfo.newBuilder(blobId).build())
+        .notify(new Callback<Blob, StorageException>() {
+          @Override
+          public void success(Blob blob) {
+            System.out.println("Batch update succeeded on " + fileName);
+          }
+
+          @Override
+          public void error(StorageException e) {
+            System.out.println("Batch update failed with cause: " + e);
+          }
+        });
+
+    batch.submit();
+  }
+
+  private static void createBucket(Storage storageClient, String bucketName) {
+    BucketInfo bucketInfo =
+        BucketInfo.newBuilder(bucketName)
+            .setLocation("us-east1")
+            .build();
+    storageClient.create(bucketInfo);
+    System.out.println("Created bucket " + bucketName);
+  }
+
+  private static void createFile(Storage storageClient, String bucketName, String fileName) {
+    BlobInfo blobInfo =
+        BlobInfo.newBuilder(bucketName, fileName)
+            .setContentType("text/plain")
+            .build();
+    storageClient.create(blobInfo, "Hello World!".getBytes());
+    System.out.println("Created file " + blobInfo.getName());
+
+    Blob blob = storageClient.get(bucketName, fileName);
+    String content = new String(blob.getContent());
+    System.out.println("Successfully wrote to file: " + content);
+  }
+}
---
+++ samples/native-image-sample/src/test/java/com/example/storage/NativeImageStorageSampleIT.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.storage;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.cloud.ServiceOptions;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.UUID;
+import org.junit.Before;
+import org.junit.Test;
+
+public class NativeImageStorageSampleIT {
+
+  private static final String NATIVE_TEST_SECRET_ID = "native-test-secret" + UUID.randomUUID();
+  private static String PROJECT_ID = ServiceOptions.getDefaultProjectId();
+  private ByteArrayOutputStream bout;
+
+  @Before
+  public void setUp() {
+    bout = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(bout));
+  }
+
+  @Test
+  public void createAndReadStorageResources() {
+    NativeImageStorageSample.main(new String[]{});
+    assertThat(bout.toString()).contains("Created bucket " + NativeImageStorageSample.BUCKET_NAME);
+    assertThat(bout.toString()).contains("Created file " + NativeImageStorageSample.FILENAME);
+    assertThat(bout.toString()).contains("Successfully wrote to file: Hello World!");
+  }
+}
