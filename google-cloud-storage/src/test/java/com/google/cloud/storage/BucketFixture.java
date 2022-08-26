2168beabc61486c0662377fdf93ff15dc7714fa3
BenWhitehead <BenWhitehead@users.noreply.github.com>
2022-08-26 09:39:16 -0400

    test: add grpc integration tests for simplistic object read/write
    Update BucketFixture to provide its own implementation of bulk object delete.
    The method in RemoteStorageHelper relies upon batch methods which aren't
    supported for grpc.

--- google-cloud-storage/src/test/java/com/google/cloud/storage/BucketFixture.java
+++ google-cloud-storage/src/test/java/com/google/cloud/storage/BucketFixture.java
@@ -18,13 +18,21 @@ package com.google.cloud.storage;
 
 import static java.util.Objects.requireNonNull;
 
+import com.google.api.gax.paging.Page;
 import com.google.cloud.storage.conformance.retry.CleanupStrategy;
-import com.google.cloud.storage.testing.RemoteStorageHelper;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
+import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
@@ -94,7 +102,30 @@ public final class BucketFixture implements TestRule {
   private void doCleanup(String bucketName, Storage s) {
     LOGGER.info("Starting bucket cleanup...");
     try {
-      RemoteStorageHelper.forceDelete(s, bucketName, 5, TimeUnit.MINUTES);
+      // TODO: probe bucket existence, a bad test could have deleted the bucket
+      Page<Blob> page1 = s.list(bucketName);
+
+      int availableProcessors = Runtime.getRuntime().availableProcessors();
+      ListeningExecutorService exec =
+          MoreExecutors.listeningDecorator(Executors.newFixedThreadPool(2 * availableProcessors));
+
+      List<ListenableFuture<DeleteResult>> futures =
+          StreamSupport.stream(page1.iterateAll().spliterator(), false)
+              .map(b -> exec.submit(() -> new DeleteResult(b.getName(), b.delete())))
+              .collect(Collectors.toList());
+
+      List<DeleteResult> deleteResults = Futures.allAsList(futures).get(5, TimeUnit.MINUTES);
+      List<DeleteResult> failedDeletes =
+          deleteResults.stream().filter(r -> !r.success).collect(Collectors.toList());
+      failedDeletes.forEach(
+          r -> LOGGER.warning(String.format("Failed to delete object %s/%s", bucketName, r.name)));
+
+      if (failedDeletes.isEmpty()) {
+        s.delete(bucketName);
+      } else {
+        LOGGER.warning("Unable to delete bucket due to previous failed object deletes");
+      }
+      exec.shutdown();
       LOGGER.info("Bucket cleanup complete");
     } catch (Exception e) {
       LOGGER.log(Level.SEVERE, e, () -> "Error during bucket cleanup.");
@@ -125,6 +156,16 @@ public final class BucketFixture implements TestRule {
     public BucketFixture build() {
       return new BucketFixture(
           requireNonNull(handle, "handle must be non null"), bucketNameFmtString, cleanupStrategy);
+    }
+  }
+
+  private static final class DeleteResult {
+    private final String name;
+    private final boolean success;
+
+    DeleteResult(String name, boolean success) {
+      this.name = name;
+      this.success = success;
     }
   }
 }
