d1293be3ff225cff90a10ac8c78144c075ac08a9
BenWhitehead <BenWhitehead@users.noreply.github.com>
2022-09-28 17:47:53 -0400

    fix: update GrpcBlobWriteChannel to share compatible behavior with BlobWriteChannel (#1676)
    Update GrpcBlobWriteChannel to always create and finalize an upload session even if `write` is never called. This change follows the behavior of BlobWriteChannel today and is done for compatibility sake.
    
    Fixes b/248605626

--- google-cloud-storage/src/test/java/com/google/cloud/storage/it/ITObjectTest.java
+++ google-cloud-storage/src/test/java/com/google/cloud/storage/it/ITObjectTest.java
@@ -1553,9 +1553,6 @@ public class ITObjectTest {
 
   @Test
   public void testAutoContentTypeWriter() throws IOException {
-    // GRPC Write Channel bug
-    // b/248605626
-    assumeTrue(clientName.startsWith("JSON"));
     testAutoContentType("writer");
   }
 
