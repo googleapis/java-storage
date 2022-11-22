449f879fd1e86e14f1bd8a0b2ad3cb3b1ac46cc4
BenWhitehead <BenWhitehead@users.noreply.github.com>
2022-11-22 10:20:35 -0500

    test: migrate ITGrpcTest to use new StorageITRunner

--- google-cloud-storage/src/test/java/com/google/cloud/storage/it/ITGrpcTest.java
+++ google-cloud-storage/src/test/java/com/google/cloud/storage/it/ITGrpcTest.java
@@ -19,12 +19,10 @@ package com.google.cloud.storage.it;
 import static com.google.common.truth.Truth.assertThat;
 
 import com.google.api.gax.paging.Page;
-import com.google.cloud.NoCredentials;
 import com.google.cloud.WriteChannel;
 import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Bucket;
-import com.google.cloud.storage.BucketFixture;
 import com.google.cloud.storage.BucketInfo;
 import com.google.cloud.storage.CopyWriter;
 import com.google.cloud.storage.HmacKey;
@@ -40,11 +38,12 @@ import com.google.cloud.storage.Storage.BucketTargetOption;
 import com.google.cloud.storage.Storage.CopyRequest;
 import com.google.cloud.storage.Storage.CreateHmacKeyOption;
 import com.google.cloud.storage.Storage.ListHmacKeysOption;
-import com.google.cloud.storage.StorageFixture;
-import com.google.cloud.storage.StorageOptions;
-import com.google.cloud.storage.conformance.retry.CleanupStrategy;
-import com.google.cloud.storage.conformance.retry.TestBench;
-import com.google.cloud.storage.testing.RemoteStorageHelper;
+import com.google.cloud.storage.TransportCompatibility.Transport;
+import com.google.cloud.storage.it.runner.StorageITRunner;
+import com.google.cloud.storage.it.runner.annotations.Backend;
+import com.google.cloud.storage.it.runner.annotations.CrossRun;
+import com.google.cloud.storage.it.runner.annotations.Inject;
+import com.google.cloud.storage.it.runner.registry.Generator;
 import com.google.common.collect.ImmutableList;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -53,64 +52,47 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.stream.IntStream;
 import java.util.stream.StreamSupport;
-import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestName;
+import org.junit.runner.RunWith;
 
+@RunWith(StorageITRunner.class)
+@CrossRun(
+    backends = {Backend.TEST_BENCH},
+    transports = {Transport.HTTP, Transport.GRPC})
 public final class ITGrpcTest {
-  @ClassRule(order = 1)
-  public static final TestBench TEST_BENCH =
-      TestBench.newBuilder().setContainerName("it-grpc").build();
 
-  @ClassRule(order = 2)
-  public static final StorageFixture storageFixture =
-      StorageFixture.from(
-          () ->
-              StorageOptions.grpc()
-                  .setHost(TEST_BENCH.getGRPCBaseUri())
-                  .setCredentials(NoCredentials.getInstance())
-                  .setProjectId("test-project-id")
-                  .build());
+  @Inject public Storage storage;
 
-  @ClassRule(order = 3)
-  public static final BucketFixture bucketFixture =
-      BucketFixture.newBuilder()
-          .setBucketNameFmtString("java-storage-grpc-%s")
-          .setCleanupStrategy(CleanupStrategy.ALWAYS)
-          .setHandle(storageFixture::getInstance)
-          .build();
+  @Inject public BucketInfo bucketInfo;
+
+  @Inject public Generator generator;
 
   @Rule public final TestName testName = new TestName();
 
   @Test
   public void testCreateBucket() {
-    final String bucketName = RemoteStorageHelper.generateBucketName();
-    Bucket bucket = storageFixture.getInstance().create(BucketInfo.of(bucketName));
+    String bucketName = generator.randomBucketName();
+    Bucket bucket = storage.create(BucketInfo.of(bucketName));
     assertThat(bucket.getName()).isEqualTo(bucketName);
   }
 
   @Test
   public void listBlobs() {
-    BucketInfo bucketInfo = bucketFixture.getBucketInfo();
     byte[] content = "Hello, World!".getBytes(StandardCharsets.UTF_8);
     String prefix = testName.getMethodName();
     List<Blob> blobs =
         IntStream.rangeClosed(1, 10)
             .mapToObj(i -> String.format("%s/%02d", prefix, i))
             .map(n -> BlobInfo.newBuilder(bucketInfo, n).build())
-            .map(
-                info ->
-                    storageFixture
-                        .getInstance()
-                        .create(info, content, BlobTargetOption.doesNotExist()))
+            .map(info -> storage.create(info, content, BlobTargetOption.doesNotExist()))
             .collect(ImmutableList.toImmutableList());
 
     List<String> expected =
         blobs.stream().map(Blob::getName).collect(ImmutableList.toImmutableList());
 
-    Page<Blob> list =
-        storageFixture.getInstance().list(bucketInfo.getName(), BlobListOption.prefix(prefix));
+    Page<Blob> list = storage.list(bucketInfo.getName(), BlobListOption.prefix(prefix));
     ImmutableList<String> actual =
         StreamSupport.stream(list.iterateAll().spliterator(), false)
             .map(Blob::getName)
@@ -121,19 +103,19 @@ public final class ITGrpcTest {
 
   @Test
   public void listBuckets() {
-    Page<Bucket> list = storageFixture.getInstance().list();
+    Page<Bucket> list = storage.list();
     ImmutableList<String> bucketNames =
         StreamSupport.stream(list.iterateAll().spliterator(), false)
             .map(Bucket::getName)
             .collect(ImmutableList.toImmutableList());
 
-    assertThat(bucketNames).contains(bucketFixture.getBucketInfo().getName());
+    assertThat(bucketNames).contains(bucketInfo.getName());
   }
 
   @Test
   public void createHmacKey() {
     ServiceAccount serviceAccount = ServiceAccount.of("x@y.z");
-    HmacKey hmacKey = storageFixture.getInstance().createHmacKey(serviceAccount);
+    HmacKey hmacKey = storage.createHmacKey(serviceAccount);
     assertThat(hmacKey).isNotNull();
     assertThat(hmacKey.getSecretKey()).isNotNull();
     assertThat(hmacKey.getMetadata().getServiceAccount()).isEqualTo(serviceAccount);
@@ -142,9 +124,8 @@ public final class ITGrpcTest {
   @Test
   public void getHmacKey() {
     ServiceAccount serviceAccount = ServiceAccount.of("x@y.z");
-    HmacKey hmacKey = storageFixture.getInstance().createHmacKey(serviceAccount);
-    HmacKeyMetadata actual =
-        storageFixture.getInstance().getHmacKey(hmacKey.getMetadata().getAccessId());
+    HmacKey hmacKey = storage.createHmacKey(serviceAccount);
+    HmacKeyMetadata actual = storage.getHmacKey(hmacKey.getMetadata().getAccessId());
     assertThat(actual).isEqualTo(hmacKey.getMetadata());
   }
 
@@ -153,34 +134,27 @@ public final class ITGrpcTest {
     ImmutableList<HmacKey> keys =
         IntStream.rangeClosed(1, 4)
             .mapToObj(i -> ServiceAccount.of(String.format("x-%d@y.z", i)))
-            .map(
-                sa ->
-                    storageFixture
-                        .getInstance()
-                        .createHmacKey(sa, CreateHmacKeyOption.projectId("proj")))
+            .map(sa -> storage.createHmacKey(sa, CreateHmacKeyOption.projectId("proj")))
             .collect(ImmutableList.toImmutableList());
 
     ImmutableList<HmacKeyMetadata> expected =
         keys.stream().map(HmacKey::getMetadata).collect(ImmutableList.toImmutableList());
 
-    Page<HmacKeyMetadata> page =
-        storageFixture.getInstance().listHmacKeys(ListHmacKeysOption.projectId("proj"));
+    Page<HmacKeyMetadata> page = storage.listHmacKeys(ListHmacKeysOption.projectId("proj"));
 
     ImmutableList<HmacKeyMetadata> actual =
         StreamSupport.stream(page.iterateAll().spliterator(), false)
             .collect(ImmutableList.toImmutableList());
 
-    assertThat(actual).isEqualTo(expected);
+    assertThat(actual).containsAtLeastElementsIn(expected);
   }
 
   @Test
   public void updateHmacKey() {
     ServiceAccount serviceAccount = ServiceAccount.of("x@y.z");
-    HmacKey hmacKey = storageFixture.getInstance().createHmacKey(serviceAccount);
+    HmacKey hmacKey = storage.createHmacKey(serviceAccount);
     HmacKeyMetadata updated =
-        storageFixture
-            .getInstance()
-            .updateHmacKeyState(hmacKey.getMetadata(), HmacKeyState.INACTIVE);
+        storage.updateHmacKeyState(hmacKey.getMetadata(), HmacKeyState.INACTIVE);
     assertThat(updated.getServiceAccount()).isEqualTo(serviceAccount);
     assertThat(updated.getState()).isEqualTo(HmacKeyState.INACTIVE);
   }
@@ -188,15 +162,15 @@ public final class ITGrpcTest {
   @Test
   public void deleteHmacKey() {
     ServiceAccount serviceAccount = ServiceAccount.of("x@y.z");
-    HmacKey hmacKey = storageFixture.getInstance().createHmacKey(serviceAccount);
-    storageFixture.getInstance().updateHmacKeyState(hmacKey.getMetadata(), HmacKeyState.INACTIVE);
-    storageFixture.getInstance().deleteHmacKey(hmacKey.getMetadata());
+    HmacKey hmacKey = storage.createHmacKey(serviceAccount);
+    storage.updateHmacKeyState(hmacKey.getMetadata(), HmacKeyState.INACTIVE);
+    storage.deleteHmacKey(hmacKey.getMetadata());
   }
 
   @Test
   public void object_writeGetRead() {
-    Storage s = storageFixture.getInstance();
-    BlobInfo info = BlobInfo.newBuilder(bucketFixture.getBucketInfo(), "writeGetRead").build();
+    Storage s = storage;
+    BlobInfo info = BlobInfo.newBuilder(bucketInfo, "writeGetRead").build();
     byte[] content = "hello, world".getBytes(StandardCharsets.UTF_8);
     s.create(info, content, BlobTargetOption.doesNotExist());
 
@@ -208,50 +182,44 @@ public final class ITGrpcTest {
 
   @Test
   public void objectWrite_storage_create() {
-    BucketInfo bucketInfo = bucketFixture.getBucketInfo();
     BlobInfo info = BlobInfo.newBuilder(bucketInfo, testName.getMethodName()).build();
     byte[] content = "Hello, World!".getBytes(StandardCharsets.UTF_8);
-    Blob blob = storageFixture.getInstance().create(info, content, BlobTargetOption.doesNotExist());
+    Blob blob = storage.create(info, content, BlobTargetOption.doesNotExist());
     byte[] actual = blob.getContent();
     assertThat(actual).isEqualTo(content);
   }
 
   @Test
   public void objectWrite_storage_create_stream() {
-    BucketInfo bucketInfo = bucketFixture.getBucketInfo();
     BlobInfo info = BlobInfo.newBuilder(bucketInfo, testName.getMethodName()).build();
     byte[] content = "Hello, World!".getBytes(StandardCharsets.UTF_8);
     Blob blob =
-        storageFixture
-            .getInstance()
-            .create(info, new ByteArrayInputStream(content), BlobWriteOption.doesNotExist());
+        storage.create(info, new ByteArrayInputStream(content), BlobWriteOption.doesNotExist());
     byte[] actual = blob.getContent();
     assertThat(actual).isEqualTo(content);
   }
 
   @Test
   public void objectWrite_storage_writer() throws IOException {
-    BucketInfo bucketInfo = bucketFixture.getBucketInfo();
     BlobInfo info = BlobInfo.newBuilder(bucketInfo, testName.getMethodName()).build();
     byte[] content = "Hello, World!".getBytes(StandardCharsets.UTF_8);
-    try (WriteChannel c =
-        storageFixture.getInstance().writer(info, BlobWriteOption.doesNotExist())) {
+    try (WriteChannel c = storage.writer(info, BlobWriteOption.doesNotExist())) {
       c.write(ByteBuffer.wrap(content));
     }
-    byte[] actual = storageFixture.getInstance().readAllBytes(info.getBlobId());
+    byte[] actual = storage.readAllBytes(info.getBlobId());
     assertThat(actual).isEqualTo(content);
   }
 
   @Test
   public void storageCopy() {
-    Storage s = storageFixture.getInstance();
+    Storage s = storage;
 
     byte[] expected = "Hello, World!".getBytes(StandardCharsets.UTF_8);
 
-    BlobInfo info = BlobInfo.newBuilder(bucketFixture.getBucketInfo(), "copy/src").build();
+    BlobInfo info = BlobInfo.newBuilder(bucketInfo, testName.getMethodName() + "copy/src").build();
     Blob cpySrc = s.create(info, expected, BlobTargetOption.doesNotExist());
 
-    BlobInfo dst = BlobInfo.newBuilder(bucketFixture.getBucketInfo(), "copy/dst").build();
+    BlobInfo dst = BlobInfo.newBuilder(bucketInfo, testName.getMethodName() + "copy/dst").build();
 
     CopyRequest copyRequest =
         CopyRequest.newBuilder()
@@ -269,10 +237,9 @@ public final class ITGrpcTest {
 
   @Test
   public void lockBucketRetentionPolicy() {
-    Storage s = storageFixture.getInstance();
+    Storage s = storage;
 
-    String tmpBucketName = bucketFixture.newBucketName();
-    Bucket bucket = s.create(BucketInfo.of(tmpBucketName));
+    Bucket bucket = s.create(BucketInfo.of(generator.randomBucketName()));
 
     Bucket locked = bucket.lockRetentionPolicy(BucketTargetOption.metagenerationMatch());
     try {
