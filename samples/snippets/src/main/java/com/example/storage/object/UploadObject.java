168f6b3ca8cdb4a56a585753a2d48dd20fceb868
Jesse Lovelace <jesselovelace@google.com>
2022-12-16 11:04:51 -0800

    add UploadObject

--- samples/snippets/src/main/java/com/example/storage/object/UploadObject.java
+++ samples/snippets/src/main/java/com/example/storage/object/UploadObject.java
@@ -41,18 +41,26 @@ public class UploadObject {
     // The path to your file to upload
     // String filePath = "path/to/your/file"
 
-    // Optional: set a generation-match precondition to avoid potential race
-    // conditions and data corruptions. The request returns a 412 error if the
-    // preconditions are not met.
-    // For a target object that does not yet exist, set the DoesNotExist precondition.
-    Storage.BlobTargetOption precondition = Storage.BlobTargetOption.doesNotExist();
-    // If the destination already exists in your bucket, instead set a generation-match
-    // precondition:
-    // Storage.BlobTargetOption precondition = Storage.BlobTargetOption.generationMatch();
-
     Storage storage = StorageOptions.newBuilder().setProjectId(projectId).build().getService();
     BlobId blobId = BlobId.of(bucketName, objectName);
     BlobInfo blobInfo = BlobInfo.newBuilder(blobId).build();
+
+    // Optional: set a generation-match precondition to avoid potential race
+    // conditions and data corruptions. The request returns a 412 error if the
+    // preconditions are not met.
+    Storage.BlobTargetOption precondition;
+    if (storage.get(bucketName, objectName) == null) {
+      // For a target object that does not yet exist, set the DoesNotExist precondition.
+      // This will cause the request to fail if the object is created before the request runs.
+      precondition = Storage.BlobTargetOption.doesNotExist();
+    } else {
+      // If the destination already exists in your bucket, instead set a generation-match
+      // precondition. This will cause the request to fail if the existing object's generation
+      // changes before the request runs.
+      precondition =
+          Storage.BlobTargetOption.generationMatch(
+              storage.get(bucketName, objectName).getGeneration());
+    }
     storage.create(blobInfo, Files.readAllBytes(Paths.get(filePath)), precondition);
 
     System.out.println(
