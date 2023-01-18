03350211eeccd2cd1b9acf73ae7afe9008695268
Sydney Munro <sydmunro@google.com>
2023-01-18 14:11:30 -0800

    Upload result initialization

--- google-cloud-storage/src/main/java/com/google/cloud/storage/transfermanager/UploadResult.java
+++ google-cloud-storage/src/main/java/com/google/cloud/storage/transfermanager/UploadResult.java
@@ -16,13 +16,118 @@
 
 package com.google.cloud.storage.transfermanager;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.StorageException;
+import com.google.common.base.MoreObjects;
+import java.util.Objects;
+import org.checkerframework.checker.nullness.qual.NonNull;
 
 public class UploadResult {
 
-  BlobInfo input;
-  TransferStatus status;
-  BlobInfo uploadedBlob;
-  StorageException exception;
+  @NonNull private final BlobInfo input;
+  @NonNull private final TransferStatus status;
+  @NonNull private final BlobInfo uploadedBlob;
+  @NonNull private final StorageException exception;
+
+  public UploadResult(
+      @NonNull BlobInfo input,
+      @NonNull TransferStatus status,
+      @NonNull BlobInfo uploadedBlob,
+      @NonNull StorageException exception) {
+    this.input = input;
+    this.status = status;
+    this.uploadedBlob = uploadedBlob;
+    this.exception = exception;
+  }
+
+  public @NonNull BlobInfo getInput() {
+    return input;
+  }
+
+  public @NonNull TransferStatus getStatus() {
+    return status;
+  }
+
+  public @NonNull BlobInfo getUploadedBlob() {
+    return uploadedBlob;
+  }
+
+  public @NonNull StorageException getException() {
+    return exception;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    UploadResult that = (UploadResult) o;
+    return input.equals(that.input) && status == that.status && uploadedBlob.equals(
+        that.uploadedBlob)
+        && exception.equals(that.exception);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(input, status, uploadedBlob, exception);
+  }
+
+  @Override
+  public String toString() {
+    return MoreObjects.toStringHelper(this)
+        .add("input", input)
+        .add("status", status)
+        .add("uploadedBlob", uploadedBlob)
+        .add("exception", exception)
+        .toString();
+  }
+
+  public static Builder newBuilder() {
+    return new Builder();
+  }
+
+  public static class Builder {
+
+    private @NonNull BlobInfo input;
+    private @NonNull TransferStatus status;
+    private @NonNull BlobInfo uploadedBlob;
+    private @NonNull StorageException exception;
+
+    private Builder() {
+      //TODO: set "NULL" values
+    }
+
+    public Builder setInput(@NonNull BlobInfo input) {
+      this.input = input;
+      return this;
+    }
+
+    public Builder setStatus(@NonNull TransferStatus status) {
+      this.status = status;
+      return this;
+    }
+
+    public Builder setUploadedBlob(@NonNull BlobInfo uploadedBlob) {
+      this.uploadedBlob = uploadedBlob;
+      return this;
+    }
+
+    public Builder setException(@NonNull StorageException exception) {
+      this.exception = exception;
+      return this;
+    }
+
+    public UploadResult build() {
+      checkNotNull(input);
+      checkNotNull(status);
+      checkNotNull(uploadedBlob);
+      checkNotNull(exception);
+      return new UploadResult(input, status, uploadedBlob, exception);
+    }
+  }
 }
