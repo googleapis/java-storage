a9fab098e23c93887f0aae8a5de6e3477d5f0f0c
BenWhitehead <BenWhitehead@users.noreply.github.com>
2023-02-01 16:30:39 -0500

    feat: add Storage.BlobWriteOption.{meta,}generation{Not,}Match(long) methods to allow literal value construction (#1875)
    #### New Methods
    * com.google.cloud.storage.Storage.BlobWriteOption.generationMatch(long)
    * com.google.cloud.storage.Storage.BlobWriteOption.generationNotMatch(long)
    * com.google.cloud.storage.Storage.BlobWriteOption.metagenerationMatch(long)
    * com.google.cloud.storage.Storage.BlobWriteOption.metagenerationNotMatch(long)
    
    Update UploadObject sample to use createFrom rather than trying to read the entire file into memory.

--- samples/snippets/src/main/java/com/example/storage/object/UploadObject.java
+++ samples/snippets/src/main/java/com/example/storage/object/UploadObject.java
@@ -48,20 +48,20 @@ public class UploadObject {
     // Optional: set a generation-match precondition to avoid potential race
     // conditions and data corruptions. The request returns a 412 error if the
     // preconditions are not met.
-    Storage.BlobTargetOption precondition;
+    Storage.BlobWriteOption precondition;
     if (storage.get(bucketName, objectName) == null) {
       // For a target object that does not yet exist, set the DoesNotExist precondition.
       // This will cause the request to fail if the object is created before the request runs.
-      precondition = Storage.BlobTargetOption.doesNotExist();
+      precondition = Storage.BlobWriteOption.doesNotExist();
     } else {
       // If the destination already exists in your bucket, instead set a generation-match
       // precondition. This will cause the request to fail if the existing object's generation
       // changes before the request runs.
       precondition =
-          Storage.BlobTargetOption.generationMatch(
+          Storage.BlobWriteOption.generationMatch(
               storage.get(bucketName, objectName).getGeneration());
     }
-    storage.create(blobInfo, Files.readAllBytes(Paths.get(filePath)), precondition);
+    storage.createFrom(blobInfo, Paths.get(filePath), precondition);
 
     System.out.println(
         "File " + filePath + " uploaded to bucket " + bucketName + " as " + objectName);
