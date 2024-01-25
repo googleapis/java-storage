fb335084c151dabf14f152f8cfdf20b8069ea04c
Frank Natividad <franknatividad@google.com>
2024-01-25 14:12:21 -0800

    add inline test project

---
+++ inline-test-project/src/main/java/org/example/ArunFailureWorkaround.java
@@ -0,0 +1,155 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.example;
+
+import com.google.auth.Credentials;
+import static com.google.auth.http.AuthHttpConstants.AUTHORIZATION;
+import com.google.auth.oauth2.AccessToken;
+import com.google.auth.oauth2.CredentialAccessBoundary;
+import com.google.auth.oauth2.DownscopedCredentials;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.NoCredentials;
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import com.google.common.collect.ImmutableList;
+import com.google.storage.v2.BucketName;
+import com.google.storage.v2.GetObjectRequest;
+import io.grpc.CallOptions;
+import io.grpc.Channel;
+import io.grpc.ClientCall;
+import io.grpc.ClientInterceptor;
+import io.grpc.ForwardingClientCall;
+import io.grpc.Metadata;
+import io.grpc.MethodDescriptor;
+
+import java.io.IOException;
+
+// -Djava.util.logging.config.file=/home/franknatividad/Documents/TestJavaStorageProject/logging.properties
+//
+public class ArunFailureWorkaround {
+    private static AccessToken getTokenFromBroker(String bucketName, String objectPrefix)
+            throws IOException {
+        // Retrieve the source credentials from ADC.
+        GoogleCredentials sourceCredentials =
+                GoogleCredentials.getApplicationDefault()
+                        .createScoped("https://www.googleapis.com/auth/cloud-platform");
+        // Initialize the Credential Access Boundary rules.
+        String availableResource = "//storage.googleapis.com/projects/_/buckets/" + bucketName;
+        // Downscoped credentials will have readonly access to the resource.
+        String availablePermission = "inRole:roles/storage.objectViewer";
+        // Only objects starting with the specified prefix string in the object name will be allowed
+        // read access.
+        String expression =
+                "resource.name.startsWith('projects/_/buckets/"
+                        + bucketName
+                        + "/objects/"
+                        + objectPrefix
+                        + "')";
+        // Build the AvailabilityCondition.
+        CredentialAccessBoundary.AccessBoundaryRule.AvailabilityCondition availabilityCondition =
+                CredentialAccessBoundary.AccessBoundaryRule.AvailabilityCondition.newBuilder()
+                        .setExpression(expression)
+                        .build();
+        // Define the single access boundary rule using the above properties.
+        CredentialAccessBoundary.AccessBoundaryRule rule =
+                CredentialAccessBoundary.AccessBoundaryRule.newBuilder()
+                        .setAvailableResource(availableResource)
+                        .addAvailablePermission(availablePermission)
+                        .setAvailabilityCondition(availabilityCondition)
+                        .build();
+        // Define the Credential Access Boundary with all the relevant rules.
+        CredentialAccessBoundary credentialAccessBoundary =
+                CredentialAccessBoundary.newBuilder().addRule(rule).build();
+        // Create the downscoped credentials.
+        DownscopedCredentials downscopedCredentials =
+                DownscopedCredentials.newBuilder()
+                        .setSourceCredential(sourceCredentials)
+                        .setCredentialAccessBoundary(credentialAccessBoundary)
+                        .build();
+        // Retrieve the token.
+        // This will need to be passed to the Token Consumer.
+        AccessToken accessToken = downscopedCredentials.refreshAccessToken();
+        return accessToken;
+    }
+    private static class DownscopedTokenByRequestInterceptor implements ClientInterceptor {
+        public final Metadata.Key<String> AUTH_KEY =
+                Metadata.Key.of(AUTHORIZATION, Metadata.ASCII_STRING_MARSHALLER);
+        @Override
+        public <ReqT, RespT> ClientCall<ReqT, RespT> interceptCall(MethodDescriptor<ReqT, RespT> method, CallOptions callOptions, Channel next) {
+            if (!method.getFullMethodName().equals("google.storage.v2.Storage/GetObject")) {
+                // Only support PCU based operations
+                return next.newCall(method, callOptions);
+            }
+            return new ForwardingClientCall.SimpleForwardingClientCall<ReqT, RespT>(next.newCall(method, callOptions)) {
+                Listener responseListener;
+                Metadata headers;
+                int flowControlRequests;
+                String methodName;
+                @Override
+                public void start(Listener<RespT> responseListener, Metadata headers) {
+                    this.responseListener = responseListener;
+                    this.headers = headers;
+                    this.methodName = method.getFullMethodName();
+                }
+                @Override
+                public void sendMessage(ReqT message) {
+                    if (headers != null) { // start() is required before sendMessage()
+                        try {
+                            GetObjectRequest req = (GetObjectRequest)message;
+                            String bucketName = BucketName.parse(req.getBucket()).getBucket();
+                            String token = getTokenFromBroker(bucketName, req.getObject()).getTokenValue();
+                            headers.put(AUTH_KEY, "Bearer " + token);
+                        } catch (Exception e) {
+                            halfClose();
+                        }
+                        delegate().start(responseListener, headers);
+                        if (flowControlRequests != 0) {
+                            super.request(flowControlRequests);
+                        }
+                        headers = null;
+                    }
+                    super.sendMessage(message);
+                }
+                @Override
+                public void request(int numMessages) {
+                    if (headers != null) {
+                        this.flowControlRequests += numMessages;
+                    } else {
+                        super.request(numMessages);
+                    }
+                }
+            };
+        }
+    }
+
+    private static Credentials getNoCredentialsWorkaround() {
+        GoogleCredentials theCred = GoogleCredentials.create(new AccessToken("", null));
+        return theCred;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("start");
+        Storage storage = StorageOptions.grpc()
+                .setAttemptDirectPath(true)
+                .setGrpcInterceptorProvider(() -> ImmutableList.of(new DownscopedTokenByRequestInterceptor()))
+                .setCredentials(getNoCredentialsWorkaround())
+                .build().getService();
+        Blob blob = storage.get("anima-frank-us-central1-gcs-grpc-team-test-bucket", "pom.xml");
+        System.out.println("Downloaded blob?: " + (blob != null));
+    }
+}
---
+++ inline-test-project/src/main/java/org/example/MemoryReadTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.example;
+
+import com.google.cloud.ReadChannel;
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hasher;
+import com.google.common.hash.Hashing;
+
+import java.nio.ByteBuffer;
+
+public class MemoryReadTest {
+
+    public static void main(String[] args) throws Exception {
+        final int numberOfReads = Integer.parseInt(args[0]);
+        final String bucketName = args[1];
+        final String objectName = args[2];
+        final String transport = args[3];
+        Storage storage;
+        if (transport.equals("grpc")) {
+             storage = StorageOptions.grpc()
+                    .setAttemptDirectPath(true)
+                    .build().getService();
+        } else {
+            storage = StorageOptions.http()
+                    .build().getService();
+        }
+        final BlobId blobId = BlobId.of(bucketName, objectName);
+//        final BlobId blobId = BlobId.of("anima-frank-gcs-grpc-team-test-central1", "50gb-sample.txt");
+        System.out.println("Starting...");
+        System.out.println("Zero-copy");
+        HashFunction hashFunction = Hashing.crc32c();
+        Hasher hasher = hashFunction.newHasher();
+        ByteBuffer buffer = ByteBuffer.allocate(1024*1024*2);
+        for (int i = 0; i < numberOfReads; i++) {
+            ReadChannel r = storage.reader(blobId);
+            int totalBytesRead = 0;
+            while (r.isOpen()) {
+                int bytesRead = r.read(buffer);
+                if (bytesRead == -1) {
+                    break;
+                }
+                totalBytesRead += bytesRead;
+                buffer.flip();
+                hasher.putBytes(buffer);
+                buffer.clear();
+            }
+            System.out.println("hasher: " + hasher.hash());
+            System.out.println("Downlaoded(" + i + "): " + totalBytesRead + ": 100MiB?" + (totalBytesRead == 1024*1024*100));
+        }
+        System.out.println("Finished...");
+    }
+}
