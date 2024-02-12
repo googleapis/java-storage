201ac94d0aa82f692edb92eeb1e2e1d614d3d0af
Sydney Munro <97561403+sydney-munro@users.noreply.github.com>
2024-02-12 09:20:01 -0800

    chore(benchmarking): Use api when determining workload runs (#2404)
    * chore(benchmarking): Use api when determining workload runs
    
    * Make api required
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    ---------
    
    Co-authored-by: Owl Bot <gcf-owl-bot[bot]@users.noreply.github.com>

--- storage-shared-benchmarking/src/main/java/com/google/cloud/storage/benchmarking/StorageSharedBenchmarkingCli.java
+++ storage-shared-benchmarking/src/main/java/com/google/cloud/storage/benchmarking/StorageSharedBenchmarkingCli.java
@@ -53,7 +53,7 @@ public final class StorageSharedBenchmarkingCli implements Runnable {
       description = "Number of workers to run in parallel for the workload")
   int workers;
 
-  @Option(names = "-api", defaultValue = "JSON", description = "API to use")
+  @Option(names = "-api", description = "API to use", required = true)
   String api;
 
   @Option(
@@ -106,15 +106,25 @@ public final class StorageSharedBenchmarkingCli implements Runnable {
       case "w1r3":
         runWorkload1();
         break;
-      case "w1r3-grpc-dp":
-        runWorkload4();
-        break;
       default:
         throw new IllegalStateException("Specify a workload to run");
     }
   }
 
   private void runWorkload1() {
+    switch (api) {
+      case "JSON":
+        runWorkload1Json();
+        break;
+      case "DirectPath":
+        runWorkload1DirectPath();
+        break;
+      default:
+        throw new IllegalStateException("Specify an API to use");
+    }
+  }
+
+  private void runWorkload1Json() {
     RetrySettings retrySettings = StorageOptions.getDefaultRetrySettings().toBuilder().build();
 
     StorageOptions retryStorageOptions =
@@ -128,7 +138,7 @@ public final class StorageSharedBenchmarkingCli implements Runnable {
     }
   }
 
-  private void runWorkload4() {
+  private void runWorkload1DirectPath() {
     RetrySettings retrySettings = StorageOptions.getDefaultRetrySettings().toBuilder().build();
     StorageOptions retryStorageOptions =
         StorageOptions.grpc().setRetrySettings(retrySettings).setAttemptDirectPath(true).build();
