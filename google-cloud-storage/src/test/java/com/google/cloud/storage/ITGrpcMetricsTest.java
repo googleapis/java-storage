261a79cd5699a0b3184d2e853243b6de2847e5db
JesseLovelace <43148100+JesseLovelace@users.noreply.github.com>
2024-06-18 13:47:41 -0700

    Apply suggestions from code review
    Co-authored-by: BenWhitehead <BenWhitehead@users.noreply.github.com>

--- google-cloud-storage/src/test/java/com/google/cloud/storage/ITGrpcMetricsTest.java
+++ google-cloud-storage/src/test/java/com/google/cloud/storage/ITGrpcMetricsTest.java
@@ -1,5 +1,7 @@
 package com.google.cloud.storage;
 
+import static com.google.common.truth.Truth.assertThat;
+
 import com.google.cloud.storage.it.runner.StorageITRunner;
 import com.google.cloud.storage.it.runner.annotations.Backend;
 import com.google.cloud.storage.it.runner.annotations.CrossRun;
@@ -10,24 +12,21 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(StorageITRunner.class)
-@CrossRun(
-    backends = {Backend.PROD},
-    transports = {TransportCompatibility.Transport.GRPC})
+@SingleBackend(Backend.PROD)
 public class ITGrpcMetricsTest {
   @Test
   public void testGrpcMetrics() {
     GlobalOpenTelemetry
         .resetForTest(); // avoids problems with "GlobalOpenTelemetry.set has already been called"
-    Storage storage = StorageOptions.grpc().build().getService();
-    Assert.assertEquals(
-        "monitoring.googleapis.com:443",
-        ((GrpcStorageOptions) storage.getOptions())
-            .getCloudMonitoringEndpoint("storage.googleapis.com:443"));
-    SdkMeterProvider provider =
-        ((GrpcStorageOptions) storage.getOptions())
-            .createMeterProvider("monitoring.googleapis.com:443");
+    GrpcStorageOptions grpcStorageOptions = StorageOptions.grpc().build();
+    assertThat(grpcStorageOptions.getCloudMonitoringEndpoint("storage.googleapis.com:443"))
+        .isEqualTo("monitoring.googleapis.com:443");
 
-    /**
+    SdkMeterProvider provider =
+        grpcStorageOptions.createMeterProvider("monitoring.googleapis.com:443");
+
+
+    /*
      * SDKMeterProvider doesn't expose the relevant fields we want to test, but they are present in
      * the String representation, so we'll check that instead. Most of the resources are auto-set,
      * and will depend on environment, which could cause flakes to check. We're only responsible for
@@ -35,16 +34,16 @@ public class ITGrpcMetricsTest {
      */
     String result = provider.toString();
 
-    Assert.assertTrue(result.contains(storage.getOptions().getProjectId()));
+    assertThat(result).contains(grpcStorageOptions.getProjectId());
 
     // This is the check for the Seconds histogram boundary. We can't practically check for every
     // boundary,
     // but if *any* are present, that means they're different from the results and we successfully
     // set them
-    Assert.assertTrue(result.contains("1.2"));
+    assertThat(result).contains("1.2");
 
     // This is the check for the Size boundary
-    Assert.assertTrue(result.contains("131072"));
+    assertThat(result).contains("131072");
   }
 
   @Test
