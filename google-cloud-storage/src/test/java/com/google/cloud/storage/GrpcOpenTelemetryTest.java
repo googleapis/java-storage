38568547f3bb7cb3d792038c8c73d6f002a4ce48
Sydney Munro <sydmunro@google.com>
2024-10-24 13:58:14 -0700

    fix checkstyle

--- google-cloud-storage/src/test/java/com/google/cloud/storage/GrpcOpenTelemetryTest.java
+++ google-cloud-storage/src/test/java/com/google/cloud/storage/GrpcOpenTelemetryTest.java
@@ -5,19 +5,20 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.google.cloud.storage;
 
 import com.google.api.core.ApiClock;
+import com.google.auth.Credentials;
+import com.google.cloud.NoCredentials;
 import com.google.cloud.storage.it.runner.StorageITRunner;
 import com.google.cloud.storage.it.runner.annotations.Backend;
 import com.google.cloud.storage.it.runner.annotations.Inject;
@@ -66,6 +67,7 @@ public class GrpcOpenTelemetryTest {
         StorageOptions.grpc()
             .setHost(testBench.getGRPCBaseUri())
             .setProjectId("projectId")
+            .setCredentials(NoCredentials.getInstance())
             .setOpenTelemetrySdk(openTelemetrySdk)
             .build();
   }
