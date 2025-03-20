googleapis/java-storage

commit e2f553788eb3f3685056728de75c358893887604

fix: update modified field handling for blob and bucket with json transport to properly clear fields (#2664)
Update StorageImpl#update(BlobInfo) and StorageImpl#update(BucketInfo) to only send modified fields in the case of an actual update. Currently, it simply sends the json version of the current info, this can mean that if a field is cleared the request to gcs doesn't actually include the field to clear.

This same issue does not impact grpc transport, because grpc transport has an explicit `update_mask` that is populated.

Fixes #2662

google-cloud-storage/src/main/java/com/google/cloud/storage/StorageImpl.java
@@ -23,6 +23,7 @@ import static com.google.common.base.Preconditions.checkState;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.concurrent.Executors.callable;
 
+import com.google.api.client.util.Data;
 import com.google.api.core.ApiFuture;
 import com.google.api.gax.paging.Page;
 import com.google.api.gax.retrying.ResultRetryAlgorithm;
@@ -44,6 +45,8 @@ import com.google.cloud.storage.PostPolicyV4.ConditionV4Type;
 import com.google.cloud.storage.PostPolicyV4.PostConditionsV4;
 import com.google.cloud.storage.PostPolicyV4.PostFieldsV4;
 import com.google.cloud.storage.PostPolicyV4.PostPolicyV4Document;
+import com.google.cloud.storage.UnifiedOpts.NamedField;
+import com.google.cloud.storage.UnifiedOpts.NestedNamedField;
 import com.google.cloud.storage.UnifiedOpts.ObjectSourceOpt;
 import com.google.cloud.storage.UnifiedOpts.ObjectTargetOpt;
 import com.google.cloud.storage.UnifiedOpts.Opts;
@@ -91,6 +94,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.stream.Stream;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
@@ -497,11 +501,35 @@ final class StorageImpl extends BaseService<StorageOptions> implements Storage, 
   public Bucket update(BucketInfo bucketInfo, BucketTargetOption... options) {
     Map<StorageRpc.Option, ?> optionsMap =
         Opts.unwrap(options).resolveFrom(bucketInfo).getRpcOptions();
-    if (bucketInfo.getModifiedFields().isEmpty()) {
+    ImmutableSet<NamedField> modifiedFields = bucketInfo.getModifiedFields();
+    if (modifiedFields.isEmpty()) {
       return internalBucketGet(bucketInfo.getName(), optionsMap);
     } else {
+      com.google.api.services.storage.model.Bucket tmp = codecs.bucketInfo().encode(bucketInfo);
       com.google.api.services.storage.model.Bucket bucketPb =
-          codecs.bucketInfo().encode(bucketInfo);
+          new com.google.api.services.storage.model.Bucket();
+      Stream.concat(modifiedFields.stream(), BucketField.REQUIRED_FIELDS.stream())
+          .map(
+              f -> {
+                if (f instanceof NestedNamedField) {
+                  return ((NestedNamedField) f).getParent();
+                } else {
+                  return f;
+                }
+              })
+          .forEach(
+              field -> {
+                String jsonName = field.getApiaryName();
+                if (tmp.containsKey(jsonName)) {
+                  bucketPb.put(jsonName, tmp.get(jsonName));
+                } else {
+                  BucketField lookup = BucketField.lookup(field);
+                  if (lookup != null) {
+                    bucketPb.put(jsonName, Data.nullOf(lookup.getJsonClass()));
+                  }
+                }
+              });
+
       ResultRetryAlgorithm<?> algorithm =
           retryAlgorithmManager.getForBucketsUpdate(bucketPb, optionsMap);
       return run(
@@ -515,7 +543,8 @@ final class StorageImpl extends BaseService<StorageOptions> implements Storage, 
   public Blob update(BlobInfo blobInfo, BlobTargetOption... options) {
     Opts<ObjectTargetOpt> opts = Opts.unwrap(options).resolveFrom(blobInfo);
     Map<StorageRpc.Option, ?> optionsMap = opts.getRpcOptions();
-    boolean unmodifiedBeforeOpts = blobInfo.getModifiedFields().isEmpty();
+    ImmutableSet<NamedField> modifiedFields = blobInfo.getModifiedFields();
+    boolean unmodifiedBeforeOpts = modifiedFields.isEmpty();
     BlobInfo.Builder builder = blobInfo.toBuilder();
 
     // This is a workaround until everything is in prod for both json and grpc.
@@ -523,7 +552,7 @@ final class StorageImpl extends BaseService<StorageOptions> implements Storage, 
     // request if it was modified, so that we don't send a null object in a
     // grpc or json request.
     // todo: b/308194853
-    if (blobInfo.getModifiedFields().contains(BlobField.RETENTION)) {
+    if (modifiedFields.contains(BlobField.RETENTION)) {
       builder.setRetention(blobInfo.getRetention());
     }
     BlobInfo updated = opts.blobInfoMapper().apply(builder).build();
@@ -531,7 +560,30 @@ final class StorageImpl extends BaseService<StorageOptions> implements Storage, 
     if (unmodifiedBeforeOpts && unmodifiedAfterOpts) {
       return internalGetBlob(blobInfo.getBlobId(), optionsMap);
     } else {
-      StorageObject pb = codecs.blobInfo().encode(updated);
+      StorageObject tmp = codecs.blobInfo().encode(updated);
+      StorageObject pb = new StorageObject();
+      Stream.concat(modifiedFields.stream(), BlobField.REQUIRED_FIELDS.stream())
+          .map(
+              f -> {
+                if (f instanceof NestedNamedField) {
+                  return ((NestedNamedField) f).getParent();
+                } else {
+                  return f;
+                }
+              })
+          .forEach(
+              field -> {
+                String jsonName = field.getApiaryName();
+                if (tmp.containsKey(jsonName)) {
+                  pb.put(jsonName, tmp.get(jsonName));
+                } else {
+                  BlobField lookup = BlobField.lookup(field);
+                  if (lookup != null) {
+                    pb.put(jsonName, Data.nullOf(lookup.getJsonClass()));
+                  }
+                }
+              });
+
       ResultRetryAlgorithm<?> algorithm = retryAlgorithmManager.getForObjectsUpdate(pb, optionsMap);
       return run(
           algorithm,

google-cloud-storage/src/main/java/com/google/cloud/storage/JsonConversions.java
@@ -83,6 +83,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Objects;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -1082,6 +1083,11 @@ final class JsonConversions {
     // various data level methods in the apiary model are hostile to ImmutableList, as it does not
     // provide a public default no args constructor. Instead, apiary uses ArrayList for all internal
     // representations of JSON Arrays.
-    return l -> l.stream().map(f).collect(Collectors.toList());
+    return l -> {
+      if (l == null) {
+        return ImmutableList.of();
+      }
+      return l.stream().filter(Objects::nonNull).map(f).collect(Collectors.toList());
+    };
   }
 }

google-cloud-storage/src/main/java/com/google/cloud/storage/Storage.java
@@ -45,11 +45,13 @@ import com.google.cloud.storage.UnifiedOpts.HmacKeyListOpt;
 import com.google.cloud.storage.UnifiedOpts.HmacKeySourceOpt;
 import com.google.cloud.storage.UnifiedOpts.HmacKeyTargetOpt;
 import com.google.cloud.storage.UnifiedOpts.NamedField;
+import com.google.cloud.storage.UnifiedOpts.NestedNamedField;
 import com.google.cloud.storage.UnifiedOpts.ObjectListOpt;
 import com.google.cloud.storage.UnifiedOpts.ObjectSourceOpt;
 import com.google.cloud.storage.UnifiedOpts.ObjectTargetOpt;
 import com.google.cloud.storage.UnifiedOpts.Opts;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Streams;
@@ -62,9 +64,11 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.nio.file.Path;
 import java.security.Key;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
@@ -73,6 +77,7 @@ import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
 
 /**
  * An interface for Google Cloud Storage.
@@ -106,80 +111,107 @@ public interface Storage extends Service<StorageOptions>, AutoCloseable {
 
   enum BucketField implements FieldSelector, NamedField {
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    ID("id", "bucket_id"),
+    ID("id", "bucket_id", String.class),
     @TransportCompatibility(Transport.HTTP)
-    SELF_LINK("selfLink"),
+    SELF_LINK("selfLink", String.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    NAME("name"),
+    NAME("name", String.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    TIME_CREATED("timeCreated", "create_time"),
+    TIME_CREATED("timeCreated", "create_time", com.google.api.client.util.DateTime.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    METAGENERATION("metageneration"),
+    METAGENERATION("metageneration", Long.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    ACL("acl"),
+    ACL("acl", ArrayList.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    DEFAULT_OBJECT_ACL("defaultObjectAcl", "default_object_acl"),
+    DEFAULT_OBJECT_ACL("defaultObjectAcl", "default_object_acl", ArrayList.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    OWNER("owner"),
+    OWNER("owner", com.google.api.services.storage.model.Bucket.Owner.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    LABELS("labels"),
+    LABELS("labels", HashMap.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    LOCATION("location"),
+    LOCATION("location", String.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    LOCATION_TYPE("locationType", "location_type"),
+    LOCATION_TYPE("locationType", "location_type", String.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    WEBSITE("website"),
+    WEBSITE("website", com.google.api.services.storage.model.Bucket.Website.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    VERSIONING("versioning"),
+    VERSIONING("versioning", com.google.api.services.storage.model.Bucket.Versioning.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    CORS("cors"),
+    CORS("cors", ArrayList.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    LIFECYCLE("lifecycle"),
+    LIFECYCLE("lifecycle", com.google.api.services.storage.model.Bucket.Lifecycle.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    STORAGE_CLASS("storageClass", "storage_class"),
+    STORAGE_CLASS("storageClass", "storage_class", String.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    ETAG("etag"),
+    ETAG("etag", String.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    ENCRYPTION("encryption"),
+    ENCRYPTION("encryption", com.google.api.services.storage.model.Bucket.Encryption.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    BILLING("billing"),
+    BILLING("billing", com.google.api.services.storage.model.Bucket.Billing.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    DEFAULT_EVENT_BASED_HOLD("defaultEventBasedHold", "default_event_based_hold"),
+    DEFAULT_EVENT_BASED_HOLD("defaultEventBasedHold", "default_event_based_hold", Boolean.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    RETENTION_POLICY("retentionPolicy", "retention_policy"),
+    RETENTION_POLICY(
+        "retentionPolicy",
+        "retention_policy",
+        com.google.api.services.storage.model.Bucket.RetentionPolicy.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    IAMCONFIGURATION("iamConfiguration", "iam_config"),
+    IAMCONFIGURATION(
+        "iamConfiguration",
+        "iam_config",
+        com.google.api.services.storage.model.Bucket.IamConfiguration.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    LOGGING("logging"),
+    LOGGING("logging", com.google.api.services.storage.model.Bucket.Logging.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    UPDATED("updated", "update_time"),
+    UPDATED("updated", "update_time", com.google.api.client.util.DateTime.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    RPO("rpo"),
+    RPO("rpo", String.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    CUSTOM_PLACEMENT_CONFIG("customPlacementConfig", "custom_placement_config"),
+    CUSTOM_PLACEMENT_CONFIG(
+        "customPlacementConfig",
+        "custom_placement_config",
+        com.google.api.services.storage.model.Bucket.CustomPlacementConfig.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    AUTOCLASS("autoclass"),
+    AUTOCLASS("autoclass", com.google.api.services.storage.model.Bucket.Autoclass.class),
 
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    HIERARCHICAL_NAMESPACE("hierarchicalNamespace", "hierarchical_namespace"),
+    HIERARCHICAL_NAMESPACE(
+        "hierarchicalNamespace",
+        "hierarchical_namespace",
+        com.google.api.services.storage.model.Bucket.HierarchicalNamespace.class),
     @TransportCompatibility({Transport.HTTP})
-    OBJECT_RETENTION("objectRetention"),
+    OBJECT_RETENTION(
+        "objectRetention", com.google.api.services.storage.model.Bucket.ObjectRetention.class),
 
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    SOFT_DELETE_POLICY("softDeletePolicy", "soft_delete_policy");
+    SOFT_DELETE_POLICY(
+        "softDeletePolicy",
+        "soft_delete_policy",
+        com.google.api.services.storage.model.Bucket.SoftDeletePolicy.class);
 
     static final List<BucketField> REQUIRED_FIELDS = ImmutableList.of(NAME);
+    private static final Map<String, BucketField> JSON_FIELD_NAME_INDEX;
+
+    static {
+      ImmutableMap.Builder<String, BucketField> tmp = ImmutableMap.builder();
+      for (BucketField field : values()) {
+        tmp.put(field.selector, field);
+      }
+      JSON_FIELD_NAME_INDEX = Utils.mapBuild(tmp);
+    }
 
     private final String selector;
     private final String grpcFieldName;
+    private final Class<?> jsonClass;
 
-    BucketField(String selector) {
-      this(selector, selector);
+    BucketField(String selector, Class<?> jsonClass) {
+      this(selector, selector, jsonClass);
     }
 
-    BucketField(String selector, String grpcFieldName) {
+    BucketField(String selector, String grpcFieldName, Class<?> jsonClass) {
       this.selector = selector;
       this.grpcFieldName = grpcFieldName;
+      this.jsonClass = jsonClass;
     }
 
     @Override
@@ -195,97 +227,130 @@ public interface Storage extends Service<StorageOptions>, AutoCloseable {
     @Override
     public String getGrpcName() {
       return grpcFieldName;
+    }
+
+    Class<?> getJsonClass() {
+      return jsonClass;
+    }
+
+    @Nullable
+    static BucketField lookup(NamedField nf) {
+      NamedField lookup = nf;
+      if (nf instanceof NestedNamedField) {
+        NestedNamedField nested = (NestedNamedField) nf;
+        lookup = nested.getParent();
+      }
+      return JSON_FIELD_NAME_INDEX.get(lookup.getApiaryName());
     }
   }
 
   enum BlobField implements FieldSelector, NamedField {
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    ACL("acl"),
+    ACL("acl", com.google.api.services.storage.model.ObjectAccessControl.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    BUCKET("bucket"),
+    BUCKET("bucket", String.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    CACHE_CONTROL("cacheControl", "cache_control"),
+    CACHE_CONTROL("cacheControl", "cache_control", String.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    COMPONENT_COUNT("componentCount", "component_count"),
+    COMPONENT_COUNT("componentCount", "component_count", Integer.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    CONTENT_DISPOSITION("contentDisposition", "content_disposition"),
+    CONTENT_DISPOSITION("contentDisposition", "content_disposition", String.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    CONTENT_ENCODING("contentEncoding", "content_encoding"),
+    CONTENT_ENCODING("contentEncoding", "content_encoding", String.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    CONTENT_LANGUAGE("contentLanguage", "content_language"),
+    CONTENT_LANGUAGE("contentLanguage", "content_language", String.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    CONTENT_TYPE("contentType", "content_type"),
+    CONTENT_TYPE("contentType", "content_type", String.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    CRC32C("crc32c", "checksums.crc32c"),
+    CRC32C("crc32c", "checksums.crc32c", String.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    ETAG("etag"),
+    ETAG("etag", String.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    GENERATION("generation"),
+    GENERATION("generation", Long.class),
     @TransportCompatibility(Transport.HTTP)
-    ID("id"),
+    ID("id", String.class),
     /** {@code kind} is not exposed in {@link BlobInfo} or {@link Blob} no need to select it */
     @Deprecated
     @TransportCompatibility(Transport.HTTP)
-    KIND("kind"),
+    KIND("kind", String.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    MD5HASH("md5Hash", "checksums.md5_hash"),
+    MD5HASH("md5Hash", "checksums.md5_hash", String.class),
     @TransportCompatibility(Transport.HTTP)
-    MEDIA_LINK("mediaLink"),
+    MEDIA_LINK("mediaLink", String.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    METADATA("metadata"),
+    METADATA("metadata", HashMap.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    METAGENERATION("metageneration"),
+    METAGENERATION("metageneration", Long.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    NAME("name"),
+    NAME("name", String.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    OWNER("owner"),
+    OWNER("owner", com.google.api.services.storage.model.StorageObject.Owner.class),
     @TransportCompatibility(Transport.HTTP)
-    SELF_LINK("selfLink"),
+    SELF_LINK("selfLink", String.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    SIZE("size"),
+    SIZE("size", java.math.BigInteger.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    STORAGE_CLASS("storageClass", "storage_class"),
+    STORAGE_CLASS("storageClass", "storage_class", String.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    TIME_DELETED("timeDeleted", "delete_time"),
+    TIME_DELETED("timeDeleted", "delete_time", com.google.api.client.util.DateTime.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    TIME_CREATED("timeCreated", "create_time"),
+    TIME_CREATED("timeCreated", "create_time", com.google.api.client.util.DateTime.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    KMS_KEY_NAME("kmsKeyName", "kms_key"),
+    KMS_KEY_NAME("kmsKeyName", "kms_key", String.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    EVENT_BASED_HOLD("eventBasedHold", "event_based_hold"),
+    EVENT_BASED_HOLD("eventBasedHold", "event_based_hold", String.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    TEMPORARY_HOLD("temporaryHold", "temporary_hold"),
+    TEMPORARY_HOLD("temporaryHold", "temporary_hold", String.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    RETENTION_EXPIRATION_TIME("retentionExpirationTime", "retention_expire_time"),
+    RETENTION_EXPIRATION_TIME(
+        "retentionExpirationTime",
+        "retention_expire_time",
+        com.google.api.client.util.DateTime.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    UPDATED("updated", "update_time"),
+    UPDATED("updated", "update_time", com.google.api.client.util.DateTime.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    CUSTOM_TIME("customTime", "custom_time"),
+    CUSTOM_TIME("customTime", "custom_time", com.google.api.client.util.DateTime.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    TIME_STORAGE_CLASS_UPDATED("timeStorageClassUpdated", "update_storage_class_time"),
+    TIME_STORAGE_CLASS_UPDATED(
+        "timeStorageClassUpdated",
+        "update_storage_class_time",
+        com.google.api.client.util.DateTime.class),
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    CUSTOMER_ENCRYPTION("customerEncryption", "customer_encryption"),
+    CUSTOMER_ENCRYPTION("customerEncryption", "customer_encryption", String.class),
     @TransportCompatibility({Transport.HTTP})
-    RETENTION("retention"),
+    RETENTION("retention", com.google.api.services.storage.model.StorageObject.Retention.class),
 
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    SOFT_DELETE_TIME("softDeleteTime", "soft_delete_time"),
+    SOFT_DELETE_TIME(
+        "softDeleteTime", "soft_delete_time", com.google.api.client.util.DateTime.class),
 
     @TransportCompatibility({Transport.HTTP, Transport.GRPC})
-    HARD_DELETE_TIME("hardDeleteTime", "hard_delete_time");
+    HARD_DELETE_TIME(
+        "hardDeleteTime", "hard_delete_time", com.google.api.client.util.DateTime.class);
 
     static final List<NamedField> REQUIRED_FIELDS = ImmutableList.of(BUCKET, NAME);
+    private static final Map<String, BlobField> JSON_FIELD_NAME_INDEX;
+
+    static {
+      ImmutableMap.Builder<String, BlobField> tmp = ImmutableMap.builder();
+      for (BlobField field : values()) {
+        tmp.put(field.selector, field);
+      }
+      JSON_FIELD_NAME_INDEX = Utils.mapBuild(tmp);
+    }
 
     private final String selector;
     private final String grpcFieldName;
+    private final Class<?> jsonClass;
 
-    BlobField(String selector) {
-      this(selector, selector);
+    BlobField(String selector, Class<?> jsonClass) {
+      this(selector, selector, jsonClass);
     }
 
-    BlobField(String selector, String grpcFieldName) {
+    BlobField(String selector, String grpcFieldName, Class<?> jsonClass) {
       this.selector = selector;
       this.grpcFieldName = grpcFieldName;
+      this.jsonClass = jsonClass;
     }
 
     @Override
@@ -301,6 +366,20 @@ public interface Storage extends Service<StorageOptions>, AutoCloseable {
     @Override
     public String getGrpcName() {
       return grpcFieldName;
+    }
+
+    Class<?> getJsonClass() {
+      return jsonClass;
+    }
+
+    @Nullable
+    static BlobField lookup(NamedField nf) {
+      NamedField lookup = nf;
+      if (nf instanceof NestedNamedField) {
+        NestedNamedField nested = (NestedNamedField) nf;
+        lookup = nested.getParent();
+      }
+      return JSON_FIELD_NAME_INDEX.get(lookup.getApiaryName());
     }
   }
 

google-cloud-storage/src/main/java/com/google/cloud/storage/UnifiedOpts.java
@@ -2301,21 +2301,6 @@ final class UnifiedOpts {
    */
   @SuppressWarnings("unchecked")
   static final class Opts<T extends Opt> {
-    private static final Function<ImmutableMap.Builder<?, ?>, ImmutableMap<?, ?>> mapBuild;
-
-    static {
-      Function<ImmutableMap.Builder<?, ?>, ImmutableMap<?, ?>> tmp;
-      // buildOrThrow was added in guava 31.0
-      // if it fails, fallback to the older build() method instead.
-      // The behavior was the same, but the new name makes the behavior clear
-      try {
-        ImmutableMap.builder().buildOrThrow();
-        tmp = ImmutableMap.Builder::buildOrThrow;
-      } catch (NoSuchMethodError e) {
-        tmp = ImmutableMap.Builder::build;
-      }
-      mapBuild = tmp;
-    }
 
     private final ImmutableList<T> opts;
 
@@ -2402,7 +2387,7 @@ final class UnifiedOpts {
     ImmutableMap<StorageRpc.Option, ?> getRpcOptions() {
       ImmutableMap.Builder<StorageRpc.Option, Object> builder =
           rpcOptionMapper().apply(ImmutableMap.builder());
-      return (ImmutableMap<StorageRpc.Option, ?>) mapBuild.apply(builder);
+      return Utils.mapBuild(builder);
     }
 
     Mapper<GrpcCallContext> grpcMetadataMapper() {
@@ -2798,7 +2783,7 @@ final class UnifiedOpts {
     }
   }
 
-  private static final class NestedNamedField implements NamedField {
+  static final class NestedNamedField implements NamedField {
     private static long serialVersionUID = -7623005572810688221L;
     private final NamedField parent;
     private final NamedField child;
@@ -2816,6 +2801,14 @@ final class UnifiedOpts {
     @Override
     public String getGrpcName() {
       return parent.getGrpcName() + "." + child.getGrpcName();
+    }
+
+    NamedField getParent() {
+      return parent;
+    }
+
+    NamedField getChild() {
+      return child;
     }
 
     @Override

google-cloud-storage/src/main/java/com/google/cloud/storage/Utils.java
@@ -26,6 +26,7 @@ import com.google.cloud.storage.Conversions.Codec;
 import com.google.cloud.storage.UnifiedOpts.NamedField;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.MapDifference;
 import com.google.common.collect.Maps;
 import com.google.common.io.BaseEncoding;
@@ -56,6 +57,21 @@ import org.checkerframework.checker.nullness.qual.Nullable;
  */
 @InternalApi
 final class Utils {
+  private static final Function<ImmutableMap.Builder<?, ?>, ImmutableMap<?, ?>> mapBuild;
+
+  static {
+    Function<ImmutableMap.Builder<?, ?>, ImmutableMap<?, ?>> tmp;
+    // buildOrThrow was added in guava 31.0
+    // if it fails, fallback to the older build() method instead.
+    // The behavior was the same, but the new name makes the behavior clear
+    try {
+      ImmutableMap.builder().buildOrThrow();
+      tmp = ImmutableMap.Builder::buildOrThrow;
+    } catch (NoSuchMethodError e) {
+      tmp = ImmutableMap.Builder::build;
+    }
+    mapBuild = tmp;
+  }
 
   static final DateTimeFormatter RFC_3339_DATE_TIME_FORMATTER =
       DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
@@ -318,5 +334,9 @@ final class Utils {
     } else {
       return ImmutableList.of(t);
     }
+  }
+
+  static <K, V> ImmutableMap<K, V> mapBuild(ImmutableMap.Builder<K, V> b) {
+    return (ImmutableMap<K, V>) mapBuild.apply(b);
   }
 }

commit 64cf731d8f6494e8deaf11e2030e1a44eae1d8ea

fix: ensure object generation is sent for Storage#update(BlobInfo) using HTTP Transport
If a generation is present in the provided BlobInfo ensure it is sent to GCS. This is
a hard requirement when using versioned buckets and trying to update a non-current
object version.

Effects all version 2.42.0 through 2.50.0

Regression introduced in #2664

Fixes #2980

google-cloud-storage/src/main/java/com/google/cloud/storage/StorageImpl.java
@@ -564,7 +564,11 @@ final class StorageImpl extends BaseService<StorageOptions> implements Storage, 
     } else {
       StorageObject tmp = codecs.blobInfo().encode(updated);
       StorageObject pb = new StorageObject();
-      Stream.concat(modifiedFields.stream(), BlobField.REQUIRED_FIELDS.stream())
+      Stream.of(
+              modifiedFields.stream(),
+              BlobField.REQUIRED_FIELDS.stream(),
+              Stream.of(BlobField.GENERATION))
+          .flatMap(s -> s) // .flatten()
           .map(
               f -> {
                 if (f instanceof NestedNamedField) {

google-cloud-storage/src/test/java/com/google/cloud/storage/it/ITObjectTest.java
@@ -98,6 +98,7 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 import javax.crypto.spec.SecretKeySpec;
+import org.checkerframework.checker.nullness.qual.NonNull;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -1581,5 +1582,29 @@ public class ITObjectTest {
     gen1 = storage.get(gen1.getBlobId());
     Blob gen2 = storage.update(gen1);
     assertThat(gen2).isEqualTo(gen1);
+  }
+
+  @Test
+  public void blob_update() throws Exception {
+    ImmutableMap<@NonNull String, @NonNull String> meta1 = ImmutableMap.of("k1", "v1");
+    ImmutableMap<@NonNull String, @NonNull String> meta2 = ImmutableMap.of("k1", "v2");
+    ImmutableMap<@NonNull String, @NonNull String> meta3 = ImmutableMap.of("k1", "v1", "k2", "n1");
+
+    String randomObjectName = generator.randomObjectName();
+    BlobInfo info1 =
+        BlobInfo.newBuilder(versionedBucket, randomObjectName).setMetadata(meta1).build();
+    BlobInfo info2 =
+        BlobInfo.newBuilder(versionedBucket, randomObjectName).setMetadata(meta2).build();
+
+    BlobInfo gen1 = storage.create(info1);
+    BlobInfo gen2 = storage.create(info2);
+
+    BlobInfo update1 = gen1.toBuilder().setMetadata(meta3).build();
+
+    BlobInfo gen1_2 = storage.update(update1);
+
+    assertAll(
+        () -> assertThat(gen1_2.getMetadata()).isEqualTo(meta3),
+        () -> assertThat(gen1_2.getGeneration()).isEqualTo(gen1.getGeneration()));
   }
 }