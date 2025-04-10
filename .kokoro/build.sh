1c81d92c80d6523b1e2cd130c535ebc8ce39bf82
diegomarquezp <diegomarquezp@google.com>
2025-04-10 23:52:28 +0000

    chore: demo of changes from https://github.com/googleapis/sdk-platform-java/pull/3737
    See the PR in sdk-platform-java for more context.
    TL;DR: we are introducing an additional Kokoro job to run native tests against GraalVM for JDK 17.0.9

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -42,10 +42,12 @@ if [[ ! -z "${GOOGLE_APPLICATION_CREDENTIALS}" && "${GOOGLE_APPLICATION_CREDENTI
     export GOOGLE_APPLICATION_CREDENTIALS=$(realpath ${KOKORO_GFILE_DIR}/${GOOGLE_APPLICATION_CREDENTIALS})
 fi
 
+
 export TEST_UNIVERSE_DOMAIN_CREDENTIAL=$(realpath ${KOKORO_GFILE_DIR}/secret_manager/client-library-test-universe-domain-credential)
 export TEST_UNIVERSE_DOMAIN=$(gcloud secrets versions access latest --project cloud-devrel-kokoro-resources --secret=client-library-test-universe-domain)
 export TEST_UNIVERSE_PROJECT_ID=$(gcloud secrets versions access latest --project cloud-devrel-kokoro-resources --secret=client-library-test-universe-project-id)
 export TEST_UNIVERSE_LOCATION=$(gcloud secrets versions access latest --project cloud-devrel-kokoro-resources --secret=client-library-test-universe-storage-location)
+
 
 RETURN_CODE=0
 set +e
@@ -75,12 +77,17 @@ integration)
       verify
     RETURN_CODE=$?
     ;;
-graalvm)
+graalvmA)
     # Run Unit and Integration Tests with Native Image
     mvn -B ${INTEGRATION_TEST_ARGS} -ntp -Pnative test
     RETURN_CODE=$?
     ;;
-graalvm17)
+graalvmB)
+    # Run Unit and Integration Tests with Native Image
+    mvn -B ${INTEGRATION_TEST_ARGS} -ntp -Pnative test
+    RETURN_CODE=$?
+    ;;
+graalvmC)
     # Run Unit and Integration Tests with Native Image
     mvn -B ${INTEGRATION_TEST_ARGS} -ntp -Pnative test
     RETURN_CODE=$?
