29d646c3b5a220ed1b1e35f85797cca4d5b8c7c2
BenWhitehead <BenWhitehead@users.noreply.github.com>
2025-04-15 18:02:52 -0400

    feat: add Storage.BlobListOption#includeTrailingDelimiter

--- google-cloud-storage/src/main/java/com/google/cloud/storage/spi/v1/HttpStorageRpc.java
+++ google-cloud-storage/src/main/java/com/google/cloud/storage/spi/v1/HttpStorageRpc.java
@@ -496,7 +496,8 @@ public class HttpStorageRpc implements StorageRpc {
               .setFields(Option.FIELDS.getString(options))
               .setUserProject(Option.USER_PROJECT.getString(options))
               .setSoftDeleted(Option.SOFT_DELETED.getBoolean(options))
-              .setIncludeFoldersAsPrefixes(Option.INCLUDE_FOLDERS_AS_PREFIXES.getBoolean(options));
+              .setIncludeFoldersAsPrefixes(Option.INCLUDE_FOLDERS_AS_PREFIXES.getBoolean(options))
+              .setIncludeTrailingDelimiter(Option.INCLUDE_TRAILING_DELIMITER.getBoolean(options));
       setExtraHeaders(list, options);
       Objects objects = list.execute();
       Iterable<StorageObject> storageObjects =
