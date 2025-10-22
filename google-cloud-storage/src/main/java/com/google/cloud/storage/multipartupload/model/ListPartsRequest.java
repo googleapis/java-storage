a35e4d6ddfd866b03b05819839ba2a634d67c00d
Shreyas Sinha <shreyassinha@google.com>
2025-10-22 17:49:13 +0000

    feat: Adding API for ListParts

---
+++ google-cloud-storage/src/main/java/com/google/cloud/storage/multipartupload/model/ListPartsRequest.java
@@ -0,0 +1,194 @@
+/*
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.storage.multipartupload.model;
+
+import com.google.common.base.MoreObjects;
+import java.util.Objects;
+
+/**
+ * Represents a request to list the parts of a multipart upload.
+ */
+public class ListPartsRequest {
+  private final String bucket;
+
+  private final String key;
+
+  private final String uploadId;
+
+  private final Integer maxParts;
+
+  private final Integer partNumberMarker;
+
+  private ListPartsRequest(Builder builder) {
+    this.bucket = builder.bucket;
+    this.key = builder.key;
+    this.uploadId = builder.uploadId;
+    this.maxParts = builder.maxParts;
+    this.partNumberMarker = builder.partNumberMarker;
+  }
+
+  /**
+   * Returns the bucket name.
+   * @return the bucket name.
+   */
+  public String bucket() {
+    return bucket;
+  }
+
+  /**
+   * Returns the object name.
+   * @return the object name.
+   */
+  public String key() {
+    return key;
+  }
+
+  /**
+   * Returns the upload ID.
+   * @return the upload ID.
+   */
+  public String uploadId() {
+    return uploadId;
+  }
+
+  /**
+   * Returns the maximum number of parts to return.
+   * @return the maximum number of parts to return.
+   */
+  public Integer getMaxParts() {
+    return maxParts;
+  }
+
+  /**
+   * Returns the part number marker.
+   * @return the part number marker.
+   */
+  public Integer getPartNumberMarker() {
+    return partNumberMarker;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof ListPartsRequest)) {
+      return false;
+    }
+    ListPartsRequest that = (ListPartsRequest) o;
+    return Objects.equals(bucket, that.bucket)
+        && Objects.equals(key, that.key)
+        && Objects.equals(uploadId, that.uploadId)
+        && Objects.equals(maxParts, that.maxParts)
+        && Objects.equals(partNumberMarker, that.partNumberMarker);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(bucket, key, uploadId, maxParts, partNumberMarker);
+  }
+
+  @Override
+  public String toString() {
+    return MoreObjects.toStringHelper(this)
+        .add("bucket", bucket)
+        .add("key", key)
+        .add("uploadId", uploadId)
+        .add("maxParts", maxParts)
+        .add("partNumberMarker", partNumberMarker)
+        .toString();
+  }
+
+  /**
+   * Returns a new builder for this class.
+   * @return a new builder for this class.
+   */
+  public static Builder builder() {
+    return new Builder();
+  }
+
+  /**
+   * A builder for {@link ListPartsRequest}.
+   */
+  public static class Builder {
+    private String bucket;
+    private String key;
+    private String uploadId;
+    private Integer maxParts;
+    private Integer partNumberMarker;
+
+    private Builder() {}
+
+    /**
+     * Sets the bucket name.
+     * @param bucket the bucket name.
+     * @return this builder.
+     */
+    public Builder bucket(String bucket) {
+      this.bucket = bucket;
+      return this;
+    }
+
+    /**
+     * Sets the object name.
+     * @param key the object name.
+     * @return this builder.
+     */
+    public Builder key(String key) {
+      this.key = key;
+      return this;
+    }
+
+    /**
+     * Sets the upload ID.
+     * @param uploadId the upload ID.
+     * @return this builder.
+     */
+    public Builder uploadId(String uploadId) {
+      this.uploadId = uploadId;
+      return this;
+    }
+
+    /**
+     * Sets the maximum number of parts to return.
+     * @param maxParts the maximum number of parts to return.
+     * @return this builder.
+     */
+    public Builder maxParts(Integer maxParts) {
+      this.maxParts = maxParts;
+      return this;
+    }
+
+    /**
+     * Sets the part number marker.
+     * @param partNumberMarker the part number marker.
+     * @return this builder.
+     */
+    public Builder partNumberMarker(Integer partNumberMarker) {
+      this.partNumberMarker = partNumberMarker;
+      return this;
+    }
+
+    /**
+     * Builds a new {@link ListPartsRequest} object.
+     * @return a new {@link ListPartsRequest} object.
+     */
+    public ListPartsRequest build() {
+      return new ListPartsRequest(this);
+    }
+  }
+}
---
+++ google-cloud-storage/src/main/java/com/google/cloud/storage/multipartupload/model/Part.java
@@ -0,0 +1,179 @@
+/*
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.storage.multipartupload.model;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.google.common.base.MoreObjects;
+import java.util.Objects;
+
+/**
+ * Represents a part of a multipart upload.
+ */
+public final class Part {
+
+  @JacksonXmlProperty(localName = "PartNumber")
+  private int partNumber;
+
+  @JacksonXmlProperty(localName = "ETag")
+  private String eTag;
+
+  @JacksonXmlProperty(localName = "Size")
+  private long size;
+
+  @JacksonXmlProperty(localName = "LastModified")
+  private String lastModified;
+
+  // for jackson
+  private Part() {}
+
+  private Part(Builder builder) {
+    this.partNumber = builder.partNumber;
+    this.eTag = builder.eTag;
+    this.size = builder.size;
+    this.lastModified = builder.lastModified;
+  }
+
+  /**
+   * Returns the part number.
+   * @return the part number.
+   */
+  public int partNumber() {
+    return partNumber;
+  }
+
+  /**
+   * Returns the ETag of the part.
+   * @return the ETag of the part.
+   */
+  public String eTag() {
+    return eTag;
+  }
+
+  /**
+   * Returns the size of the part.
+   * @return the size of the part.
+   */
+  public long size() {
+    return size;
+  }
+
+  /**
+   * Returns the last modified time of the part.
+   * @return the last modified time of the part.
+   */
+  public String lastModified() {
+    return lastModified;
+  }
+
+  /**
+   * Returns a new builder for this class.
+   * @return a new builder for this class.
+   */
+  public static Builder builder() {
+    return new Builder();
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof Part)) {
+      return false;
+    }
+    Part that = (Part) o;
+    return Objects.equals(partNumber, that.partNumber)
+        && Objects.equals(eTag, that.eTag)
+        && Objects.equals(size, that.size)
+        && Objects.equals(lastModified, that.lastModified);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(partNumber, eTag, size, lastModified);
+  }
+
+  @Override
+  public String toString() {
+    return MoreObjects.toStringHelper(this)
+        .add("partNumber", partNumber)
+        .add("eTag", eTag)
+        .add("size", size)
+        .add("lastModified", lastModified)
+        .toString();
+  }
+
+  /**
+   * A builder for {@link Part}.
+   */
+  public static final class Builder {
+    private int partNumber;
+    private String eTag;
+    private long size;
+    private String lastModified;
+
+    private Builder() {}
+
+    /**
+     * Sets the part number.
+     * @param partNumber the part number.
+     * @return this builder.
+     */
+    public Builder partNumber(int partNumber) {
+      this.partNumber = partNumber;
+      return this;
+    }
+
+    /**
+     * Sets the ETag of the part.
+     * @param eTag the ETag of the part.
+     * @return this builder.
+     */
+    public Builder eTag(String eTag) {
+      this.eTag = eTag;
+      return this;
+    }
+
+    /**
+     * Sets the size of the part.
+     * @param size the size of the part.
+     * @return this builder.
+     */
+    public Builder size(long size) {
+      this.size = size;
+      return this;
+    }
+
+    /**
+     * Sets the last modified time of the part.
+     * @param lastModified the last modified time of the part.
+     * @return this builder.
+     */
+    public Builder lastModified(String lastModified) {
+      this.lastModified = lastModified;
+      return this;
+    }
+
+    /**
+     * Builds a new {@link Part} object.
+     * @return a new {@link Part} object.
+     */
+    public Part build() {
+      return new Part(this);
+    }
+  }
+}
