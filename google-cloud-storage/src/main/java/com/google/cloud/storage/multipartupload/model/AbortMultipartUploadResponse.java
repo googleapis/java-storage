e0cd719083fdd4ebc559418cf6eaf5976b5b7955
Shreyas Sinha <shreyassinha@google.com>
2025-10-22 19:03:03 +0000

    feat: Adding API for AbortMultipartUpload.

---
+++ google-cloud-storage/src/main/java/com/google/cloud/storage/multipartupload/model/AbortMultipartUploadResponse.java
@@ -0,0 +1,12 @@
+package com.google.cloud.storage.multipartupload.model;
+
+import com.google.api.core.BetaApi;
+
+/**
+ * Represents a response to an abort multipart upload request. This class is currently empty as the
+ * abort operation does not return any specific data in its response body.
+ */
+@BetaApi
+public final class AbortMultipartUploadResponse {
+
+}
