160fa9af7aa492373a9d9b40f65a6c56d7cab5ef
Shreyas Sinha <shreyassinha@google.com>
2025-11-03 21:06:41 +0000

    chore: mpu client preview merge train
    BEGIN_NESTED_COMMIT
    
    BEGIN_COMMIT_OVERRIDE
    feat: add preview MultipartUploadClient#createMultipartUpload #3356
    END_COMMIT_OVERRIDE
    
    BEGIN_COMMIT_OVERRIDE
    feat: add preview MultipartUploadClient#listParts #3359
    END_COMMIT_OVERRIDE
    
    BEGIN_COMMIT_OVERRIDE
    feat: add preview MultipartUploadClient#abortMultipartUpload #3361
    END_COMMIT_OVERRIDE
    
    BEGIN_COMMIT_OVERRIDE
    feat: add preview MultipartUploadClient#uploadPart #3375
    END_COMMIT_OVERRIDE
    
    BEGIN_COMMIT_OVERRIDE
    feat: add preview MultipartUploadClient#completeMultipartUpload #3372
    END_COMMIT_OVERRIDE
    
    BEGIN_COMMIT_OVERRIDE
    feat: add preview MultipartUploadSettings
    END_COMMIT_OVERRIDE
    
    BEGIN_COMMIT_OVERRIDE
    END_COMMIT_OVERRIDE
    
    END_NESTED_COMMIT
    
    Other changes:
    1. chore: refactor retrier creation from HttpStorageOptions to StorageOptions #3350
    2. chore: refactorings for CreateMultipartUpload #3364
    3. chore: fix xml parsing of StringEnumValue's so that the enum contract is not broken #3377
    4. chore: add PredefinedAcl#xmlEntry
    
    Co-authored-by: BenWhitehead <BenWhitehead@users.noreply.github.com>

---
+++ google-cloud-storage/src/main/java/com/google/cloud/storage/MultipartUploadHttpRequestManager.java
@@ -0,0 +1,276 @@
+/*
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.storage;
+
+import static com.google.cloud.storage.Utils.ifNonNull;
+
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.google.api.client.http.ByteArrayContent;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpHeaders;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestFactory;
+import com.google.api.client.http.UriTemplate;
+import com.google.api.client.util.ObjectParser;
+import com.google.api.gax.core.GaxProperties;
+import com.google.api.gax.rpc.FixedHeaderProvider;
+import com.google.api.gax.rpc.HeaderProvider;
+import com.google.api.services.storage.Storage;
+import com.google.cloud.storage.Crc32cValue.Crc32cLengthKnown;
+import com.google.cloud.storage.multipartupload.model.AbortMultipartUploadRequest;
+import com.google.cloud.storage.multipartupload.model.AbortMultipartUploadResponse;
+import com.google.cloud.storage.multipartupload.model.CompleteMultipartUploadRequest;
+import com.google.cloud.storage.multipartupload.model.CompleteMultipartUploadResponse;
+import com.google.cloud.storage.multipartupload.model.CreateMultipartUploadRequest;
+import com.google.cloud.storage.multipartupload.model.CreateMultipartUploadResponse;
+import com.google.cloud.storage.multipartupload.model.ListPartsRequest;
+import com.google.cloud.storage.multipartupload.model.ListPartsResponse;
+import com.google.cloud.storage.multipartupload.model.UploadPartRequest;
+import com.google.cloud.storage.multipartupload.model.UploadPartResponse;
+import com.google.common.base.StandardSystemProperty;
+import com.google.common.collect.ImmutableMap;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+final class MultipartUploadHttpRequestManager {
+
+  private final HttpRequestFactory requestFactory;
+  private final ObjectParser objectParser;
+  private final HeaderProvider headerProvider;
+
+  MultipartUploadHttpRequestManager(
+      HttpRequestFactory requestFactory, ObjectParser objectParser, HeaderProvider headerProvider) {
+    this.requestFactory = requestFactory;
+    this.objectParser = objectParser;
+    this.headerProvider = headerProvider;
+  }
+
+  CreateMultipartUploadResponse sendCreateMultipartUploadRequest(
+      URI uri, CreateMultipartUploadRequest request) throws IOException {
+
+    String createUri =
+        UriTemplate.expand(
+            uri.toString() + "{bucket}/{key}?uploads",
+            ImmutableMap.of("bucket", request.bucket(), "key", request.key()),
+            false);
+
+    HttpRequest httpRequest =
+        requestFactory.buildPostRequest(
+            new GenericUrl(createUri), new ByteArrayContent(request.getContentType(), new byte[0]));
+    httpRequest.getHeaders().putAll(headerProvider.getHeaders());
+    addHeadersForCreateMultipartUpload(request, httpRequest.getHeaders());
+    httpRequest.setParser(objectParser);
+    httpRequest.setThrowExceptionOnExecuteError(true);
+    return httpRequest.execute().parseAs(CreateMultipartUploadResponse.class);
+  }
+
+  ListPartsResponse sendListPartsRequest(URI uri, ListPartsRequest request) throws IOException {
+
+    ImmutableMap.Builder<String, Object> params =
+        ImmutableMap.<String, Object>builder()
+            .put("bucket", request.bucket())
+            .put("key", request.key())
+            .put("uploadId", request.uploadId());
+    if (request.getMaxParts() != null) {
+      params.put("max-parts", request.getMaxParts());
+    }
+    if (request.getPartNumberMarker() != null) {
+      params.put("part-number-marker", request.getPartNumberMarker());
+    }
+
+    String listUri =
+        UriTemplate.expand(
+            uri.toString() + "{bucket}/{key}{?uploadId,max-parts,part-number-marker}",
+            params.build(),
+            false);
+    HttpRequest httpRequest = requestFactory.buildGetRequest(new GenericUrl(listUri));
+    httpRequest.getHeaders().putAll(headerProvider.getHeaders());
+    httpRequest.setParser(objectParser);
+    httpRequest.setThrowExceptionOnExecuteError(true);
+    return httpRequest.execute().parseAs(ListPartsResponse.class);
+  }
+
+  AbortMultipartUploadResponse sendAbortMultipartUploadRequest(
+      URI uri, AbortMultipartUploadRequest request) throws IOException {
+
+    String abortUri =
+        UriTemplate.expand(
+            uri.toString() + "{bucket}/{key}{?uploadId}",
+            ImmutableMap.of(
+                "bucket", request.bucket(), "key", request.key(), "uploadId", request.uploadId()),
+            false);
+
+    HttpRequest httpRequest = requestFactory.buildDeleteRequest(new GenericUrl(abortUri));
+    httpRequest.getHeaders().putAll(headerProvider.getHeaders());
+    httpRequest.setParser(objectParser);
+    httpRequest.setThrowExceptionOnExecuteError(true);
+    return httpRequest.execute().parseAs(AbortMultipartUploadResponse.class);
+  }
+
+  CompleteMultipartUploadResponse sendCompleteMultipartUploadRequest(
+      URI uri, CompleteMultipartUploadRequest request) throws IOException {
+    String completeUri =
+        UriTemplate.expand(
+            uri.toString() + "{bucket}/{key}{?uploadId}",
+            ImmutableMap.of(
+                "bucket", request.bucket(), "key", request.key(), "uploadId", request.uploadId()),
+            false);
+    byte[] bytes = new XmlMapper().writeValueAsBytes(request.multipartUpload());
+    HttpRequest httpRequest =
+        requestFactory.buildPostRequest(
+            new GenericUrl(completeUri), new ByteArrayContent("application/xml", bytes));
+    httpRequest.getHeaders().putAll(headerProvider.getHeaders());
+    @Nullable Crc32cLengthKnown crc32cValue = Hasher.defaultHasher().hash(ByteBuffer.wrap(bytes));
+    addChecksumHeader(crc32cValue, httpRequest.getHeaders());
+    httpRequest.setParser(objectParser);
+    httpRequest.setThrowExceptionOnExecuteError(true);
+    return ChecksumResponseParser.parseCompleteResponse(httpRequest.execute());
+  }
+
+  UploadPartResponse sendUploadPartRequest(
+      URI uri, UploadPartRequest request, RewindableContent rewindableContent) throws IOException {
+    String uploadUri =
+        UriTemplate.expand(
+            uri.toString() + "{bucket}/{key}{?partNumber,uploadId}",
+            ImmutableMap.of(
+                "bucket",
+                request.bucket(),
+                "key",
+                request.key(),
+                "partNumber",
+                request.partNumber(),
+                "uploadId",
+                request.uploadId()),
+            false);
+    HttpRequest httpRequest =
+        requestFactory.buildPutRequest(new GenericUrl(uploadUri), rewindableContent);
+    httpRequest.getHeaders().putAll(headerProvider.getHeaders());
+    addChecksumHeader(rewindableContent.getCrc32c(), httpRequest.getHeaders());
+    httpRequest.setThrowExceptionOnExecuteError(true);
+    return ChecksumResponseParser.parseUploadResponse(httpRequest.execute());
+  }
+
+  @SuppressWarnings("DataFlowIssue")
+  static MultipartUploadHttpRequestManager createFrom(HttpStorageOptions options) {
+    Storage storage = options.getStorageRpcV1().getStorage();
+    ImmutableMap.Builder<String, String> stableHeaders =
+        ImmutableMap.<String, String>builder()
+            // http-java-client will automatically append its own version to the user-agent
+            .put("User-Agent", "gcloud-java/" + options.getLibraryVersion())
+            .put(
+                "x-goog-api-client",
+                String.format(
+                    "gl-java/%s gccl/%s %s/%s",
+                    GaxProperties.getJavaVersion(),
+                    options.getLibraryVersion(),
+                    formatName(StandardSystemProperty.OS_NAME.value()),
+                    formatSemver(StandardSystemProperty.OS_VERSION.value())));
+    ifNonNull(options.getProjectId(), pid -> stableHeaders.put("x-goog-user-project", pid));
+    return new MultipartUploadHttpRequestManager(
+        storage.getRequestFactory(),
+        new XmlObjectParser(new XmlMapper()),
+        options.getMergedHeaderProvider(FixedHeaderProvider.create(stableHeaders.build())));
+  }
+
+  private void addChecksumHeader(@Nullable Crc32cLengthKnown crc32c, HttpHeaders headers) {
+    if (crc32c != null) {
+      headers.put("x-goog-hash", "crc32c=" + Utils.crc32cCodec.encode(crc32c.getValue()));
+    }
+  }
+
+  private void addHeadersForCreateMultipartUpload(
+      CreateMultipartUploadRequest request, HttpHeaders headers) {
+    if (request.getCannedAcl() != null) {
+      headers.put("x-goog-acl", request.getCannedAcl().getXmlEntry());
+    }
+    if (request.getMetadata() != null) {
+      for (Map.Entry<String, String> entry : request.getMetadata().entrySet()) {
+        if (entry.getKey() != null || entry.getValue() != null) {
+          headers.put("x-goog-meta-" + urlEncode(entry.getKey()), urlEncode(entry.getValue()));
+        }
+      }
+    }
+    if (request.getContentType() != null) {
+      headers.put("Content-Type", request.getContentType());
+    }
+    if (request.getStorageClass() != null) {
+      headers.put("x-goog-storage-class", request.getStorageClass().toString());
+    }
+    if (request.getKmsKeyName() != null && !request.getKmsKeyName().isEmpty()) {
+      headers.put("x-goog-encryption-kms-key-name", request.getKmsKeyName());
+    }
+    if (request.getObjectLockMode() != null) {
+      headers.put("x-goog-object-lock-mode", request.getObjectLockMode().toString());
+    }
+    if (request.getObjectLockRetainUntilDate() != null) {
+      headers.put(
+          "x-goog-object-lock-retain-until-date",
+          Utils.offsetDateTimeRfc3339Codec.encode(request.getObjectLockRetainUntilDate()));
+    }
+    if (request.getCustomTime() != null) {
+      headers.put(
+          "x-goog-custom-time", Utils.offsetDateTimeRfc3339Codec.encode(request.getCustomTime()));
+    }
+  }
+
+  private static String urlEncode(String value) {
+    try {
+      return URLEncoder.encode(value, StandardCharsets.UTF_8.name());
+    } catch (UnsupportedEncodingException e) {
+      throw new StorageException(0, "Unable to load UTF-8 charset for encoding", e);
+    }
+  }
+
+  /**
+   * copied from
+   * com.google.api.client.googleapis.services.AbstractGoogleClientRequest.ApiClientVersion#formatName(java.lang.String)
+   */
+  private static String formatName(String name) {
+    // Only lowercase letters, digits, and "-" are allowed
+    return name.toLowerCase().replaceAll("[^\\w\\d\\-]", "-");
+  }
+
+  private static String formatSemver(String version) {
+    return formatSemver(version, version);
+  }
+
+  /**
+   * copied from
+   * com.google.api.client.googleapis.services.AbstractGoogleClientRequest.ApiClientVersion#formatSemver(java.lang.String,
+   * java.lang.String)
+   */
+  private static String formatSemver(String version, String defaultValue) {
+    if (version == null) {
+      return null;
+    }
+
+    // Take only the semver version: x.y.z-a_b_c -> x.y.z
+    Matcher m = Pattern.compile("(\\d+\\.\\d+\\.\\d+).*").matcher(version);
+    if (m.find()) {
+      return m.group(1);
+    } else {
+      return defaultValue;
+    }
+  }
+}
---
+++ google-cloud-storage/src/main/java/com/google/cloud/storage/multipartupload/model/UploadPartRequest.java
@@ -0,0 +1,206 @@
+/*
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.storage.multipartupload.model;
+
+import com.google.api.core.BetaApi;
+import com.google.common.base.MoreObjects;
+import java.util.Objects;
+
+/**
+ * An object to represent an upload part request. An upload part request is used to upload a single
+ * part of a multipart upload.
+ *
+ * @since 2.60.0 This new api is in preview and is subject to breaking changes.
+ */
+@BetaApi
+public final class UploadPartRequest {
+
+  private final String bucket;
+  private final String key;
+  private final int partNumber;
+  private final String uploadId;
+
+  private UploadPartRequest(Builder builder) {
+    this.bucket = builder.bucket;
+    this.key = builder.key;
+    this.partNumber = builder.partNumber;
+    this.uploadId = builder.uploadId;
+  }
+
+  /**
+   * Returns the bucket to upload the part to.
+   *
+   * @return The bucket to upload the part to.
+   * @since 2.60.0 This new api is in preview and is subject to breaking changes.
+   */
+  @BetaApi
+  public String bucket() {
+    return bucket;
+  }
+
+  /**
+   * Returns the key of the object to upload the part to.
+   *
+   * @return The key of the object to upload the part to.
+   * @since 2.60.0 This new api is in preview and is subject to breaking changes.
+   */
+  @BetaApi
+  public String key() {
+    return key;
+  }
+
+  /**
+   * Returns the part number of the part to upload.
+   *
+   * @return The part number of the part to upload.
+   * @since 2.60.0 This new api is in preview and is subject to breaking changes.
+   */
+  @BetaApi
+  public int partNumber() {
+    return partNumber;
+  }
+
+  /**
+   * Returns the upload ID of the multipart upload.
+   *
+   * @return The upload ID of the multipart upload.
+   * @since 2.60.0 This new api is in preview and is subject to breaking changes.
+   */
+  @BetaApi
+  public String uploadId() {
+    return uploadId;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof UploadPartRequest)) {
+      return false;
+    }
+    UploadPartRequest that = (UploadPartRequest) o;
+    return partNumber == that.partNumber
+        && Objects.equals(bucket, that.bucket)
+        && Objects.equals(key, that.key)
+        && Objects.equals(uploadId, that.uploadId);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(bucket, key, partNumber, uploadId);
+  }
+
+  @Override
+  public String toString() {
+    return MoreObjects.toStringHelper(this)
+        .add("bucket", bucket)
+        .add("key", key)
+        .add("partNumber", partNumber)
+        .add("uploadId", uploadId)
+        .toString();
+  }
+
+  /**
+   * Returns a new builder for an {@link UploadPartRequest}.
+   *
+   * @return A new builder.
+   * @since 2.60.0 This new api is in preview and is subject to breaking changes.
+   */
+  @BetaApi
+  public static Builder builder() {
+    return new Builder();
+  }
+
+  /**
+   * A builder for {@link UploadPartRequest}.
+   *
+   * @since 2.60.0 This new api is in preview and is subject to breaking changes.
+   */
+  @BetaApi
+  public static class Builder {
+    private String bucket;
+    private String key;
+    private int partNumber;
+    private String uploadId;
+
+    private Builder() {}
+
+    /**
+     * Sets the bucket to upload the part to.
+     *
+     * @param bucket The bucket to upload the part to.
+     * @return This builder.
+     * @since 2.60.0 This new api is in preview and is subject to breaking changes.
+     */
+    @BetaApi
+    public Builder bucket(String bucket) {
+      this.bucket = bucket;
+      return this;
+    }
+
+    /**
+     * Sets the key of the object to upload the part to.
+     *
+     * @param key The key of the object to upload the part to.
+     * @return This builder.
+     * @since 2.60.0 This new api is in preview and is subject to breaking changes.
+     */
+    @BetaApi
+    public Builder key(String key) {
+      this.key = key;
+      return this;
+    }
+
+    /**
+     * Sets the part number of the part to upload.
+     *
+     * @param partNumber The part number of the part to upload.
+     * @return This builder.
+     * @since 2.60.0 This new api is in preview and is subject to breaking changes.
+     */
+    @BetaApi
+    public Builder partNumber(int partNumber) {
+      this.partNumber = partNumber;
+      return this;
+    }
+
+    /**
+     * Sets the upload ID of the multipart upload.
+     *
+     * @param uploadId The upload ID of the multipart upload.
+     * @return This builder.
+     * @since 2.60.0 This new api is in preview and is subject to breaking changes.
+     */
+    @BetaApi
+    public Builder uploadId(String uploadId) {
+      this.uploadId = uploadId;
+      return this;
+    }
+
+    /**
+     * Builds the {@link UploadPartRequest}.
+     *
+     * @return The built {@link UploadPartRequest}.
+     * @since 2.60.0 This new api is in preview and is subject to breaking changes.
+     */
+    @BetaApi
+    public UploadPartRequest build() {
+      return new UploadPartRequest(this);
+    }
+  }
+}
