63178067039efac31c7e29d7ac1f7db4ea59ffa4
nidhiii-27 <nidhiii@google.com>
2025-11-04 18:12:15 +0530

    edit AbortMultipartUpload sample

--- samples/snippets/src/main/java/com/example/storage/multipartupload/AbortMultipartUpload.java
+++ samples/snippets/src/main/java/com/example/storage/multipartupload/AbortMultipartUpload.java
@@ -22,11 +22,10 @@ import com.google.cloud.storage.HttpStorageOptions;
 import com.google.cloud.storage.MultipartUploadClient;
 import com.google.cloud.storage.MultipartUploadSettings;
 import com.google.cloud.storage.multipartupload.model.AbortMultipartUploadRequest;
-import com.google.cloud.storage.multipartupload.model.CreateMultipartUploadRequest;
-import com.google.cloud.storage.multipartupload.model.CreateMultipartUploadResponse;
 
 public class AbortMultipartUpload {
-  public static void abortMultipartUpload(String projectId, String bucketName, String objectName) {
+  public static void abortMultipartUpload(
+      String projectId, String bucketName, String objectName, String uploadId) {
     // The ID of your GCP project
     // String projectId = "your-project-id";
 
@@ -36,16 +35,13 @@ public class AbortMultipartUpload {
     // The ID of your GCS object
     // String objectName = "your-object-name";
 
+    // The ID of the multipart upload
+    // String uploadId = "your-upload-id";
+
     HttpStorageOptions storageOptions =
         HttpStorageOptions.newBuilder().setProjectId(projectId).build();
     MultipartUploadSettings mpuSettings = MultipartUploadSettings.of(storageOptions);
     MultipartUploadClient mpuClient = MultipartUploadClient.create(mpuSettings);
-
-    System.out.println("Initiating a multipart upload to abort for " + objectName);
-    CreateMultipartUploadRequest createRequest =
-        CreateMultipartUploadRequest.builder().bucket(bucketName).key(objectName).build();
-    CreateMultipartUploadResponse createResponse = mpuClient.createMultipartUpload(createRequest);
-    String uploadId = createResponse.uploadId().trim();
 
     System.out.println("Aborting multipart upload: " + uploadId);
     AbortMultipartUploadRequest abortRequest =
