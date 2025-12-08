2ee57d9c6b7579ee5c5298f5fa79bef0684687a4
Dhriti Chopra <dhritichopra@google.com>
2025-12-08 11:08:33 +0530

    Updating according to base pr test changes

--- google-cloud-storage/src/test/java/com/google/cloud/storage/it/ITListBucketTest.java
+++ google-cloud-storage/src/test/java/com/google/cloud/storage/it/ITListBucketTest.java
@@ -31,23 +31,16 @@ import com.google.cloud.storage.it.runner.annotations.BucketType;
 import com.google.cloud.storage.it.runner.annotations.CrossRun;
 import com.google.cloud.storage.it.runner.annotations.Inject;
 import com.google.cloud.storage.it.runner.registry.Generator;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Iterables;
 import com.google.common.collect.ImmutableMap;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.StreamSupport;
 import java.util.Map;
 import java.util.stream.Collectors;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(StorageITRunner.class)
 @CrossRun(
     backends = {Backend.TEST_BENCH},
-    transports = {Transport.GRPC, Transport.HTTP})
+    transports = {Transport.HTTP, Transport.GRPC})
 
 public class ITListBucketTest {
   @Inject public Storage storage;
@@ -105,52 +98,5 @@ public class ITListBucketTest {
         return Reachable;
       }
     }
-  }
-
-  @Test
-  public void testListBucketWithPartialSuccessPagination() {
-    // Create additional buckets to force pagination
-    // We already have NORMAL_BUCKET_NAME and UNREACHABLE_BUCKET_NAME
-    // Total 2 buckets. If we set pageSize=1, we should get at least 2 pages.
-    // To be safe and ensure we have enough data, let's create a few more normal buckets.
-    createBucket("normal_bucket_2");
-    createBucket("normal_bucket_3");
-
-    Page<Bucket> page =
-        storage.list(
-            Storage.BucketListOption.returnPartialSuccess(true),
-            Storage.BucketListOption.pageSize(1));
-
-    List<Bucket> allBuckets = new ArrayList<>();
-    page.iterateAll().forEach(allBuckets::add);
-
-    // Verify we found all expected buckets
-    ImmutableList<String> bucketNames =
-        allBuckets.stream().map(Bucket::getName).collect(ImmutableList.toImmutableList());
-
-    assertThat(bucketNames)
-        .containsExactly(
-            NORMAL_BUCKET_NAME,
-            "normal_bucket_2",
-            "normal_bucket_3",
-            EXPECTED_UNREACHABLE_BUCKET_NAME);
-
-    // Verify unreachable bucket is present and marked correctly
-    Bucket actualUnreachableBucket =
-        allBuckets.stream()
-            .filter(b -> b.getName().contains(UNREACHABLE_BUCKET_NAME))
-            .findFirst()
-            .orElseThrow(() -> new AssertionError("Unreachable bucket not found in list"));
-
-    assertThat(actualUnreachableBucket.getName()).isEqualTo(EXPECTED_UNREACHABLE_BUCKET_NAME);
-    assertTrue(
-        "The unreachable bucket must have the isUnreachable flag set to true",
-        actualUnreachableBucket.isUnreachable());
-    assertThat(actualUnreachableBucket.getOwner()).isNull();
-
-    // Verify normal buckets are NOT unreachable
-    allBuckets.stream()
-        .filter(b -> !b.getName().contains(UNREACHABLE_BUCKET_NAME))
-        .forEach(b -> assertThat(b.isUnreachable()).isNull());
   }
 }
